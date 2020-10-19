02e194de8f4d41964b5365331f16c793637d1753
amooHashem <amoo.hashem.mehraban@gmail.com>
2020-10-19 03:55:43 +0330

    build card

---
+++ src/components/Cards/Workshop.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react'
+import {
+  Button,
+  Container,
+  makeStyles,
+  Tab,
+  Tabs,
+  Grid,
+  Paper,
+  Typography,
+  ButtonGroup,
+  CssBaseline,
+  CardContent,
+  CardActionArea,
+  Card,
+  CardMedia,
+  CardActions,
+  Chip,
+} from '@material-ui/core'
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  icon: {
+    textAlign: 'center',
+  }
+});
+
+const WorkShopCard = ({ name, description, teamNo, mentorNo }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea disabled>
+        <CardMedia
+          className={classes.media}
+          image={process.env.PUBLIC_URL + '/ai.jpg'}
+          title={name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+        <Grid container direction='row' justify='center'>
+          <Grid container item xs={6} justify='center'>
+            <Chip variant="outlined" icon={<EmojiPeopleIcon />} label={`${mentorNo} منتور`} />
+          </Grid>
+          <Grid container item xs={6} justify='center'>
+            <Chip variant="outlined" icon={<PeopleAltIcon />} label={`${teamNo} تیم`} />
+          </Grid>
+        </Grid>
+      </CardActionArea>
+      <CardActions>
+        <Button variant='outlined' fullWidth color="primary">
+          مشاهده
+        </Button>
+      </CardActions>
+    </Card >
+  );
+}
+
+export default WorkShopCard;
--- src/containers/MentorPage.js
+++ src/containers/MentorPage.js
@@ -11,43 +11,52 @@ import {
   ButtonGroup,
   CssBaseline,
   Hidden,
+  Backdrop,
+  Badge,
+  IconButton,
+  Tooltip,
 } from '@material-ui/core'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import GroupIcon from '@material-ui/icons/Group';
 import ClassIcon from '@material-ui/icons/Class';
 import { connect } from 'react-redux'
+import WorkShpoCard from '../components/Cards/Workshop'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  container: {
     padding: theme.spacing(2),
   },
   rightBox: {
     padding: theme.spacing(2),
-  }
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+  absolute: {
+    position: 'absolute',
+    right: theme.spacing(2),
+  },
 }));
 
 
-
-const BackButton = ({ }) => {
+const BackButton = () => {
   return (
     <Grid item>
-      <Button variant='outlined' fullWidth color='primary' startIcon={<ExitToAppIcon />}>
+      <Button variant='contained' fullWidth color='primary' startIcon={<ExitToAppIcon />}>
         بازگشت
       </Button>
     </Grid>
   )
 }
 
-const MentorPage = ({ }) => {
+const MentorPage = ({ mentorRequestNo, isLoading }) => {
   const classes = useStyles();
-  const [tabNumber, setTabNumber] = useState(0);
-
-
-  console.log(tabNumber)
-
 
   return (
-    <Container className={classes.root}>
+    <Container className={classes.container}>
       <CssBaseline />
       <Grid container spacing={2} direction='row' justify='space-around'>
         <Grid container item sm={3} xs={12} direction='column' justify='space-between'>
@@ -59,7 +68,11 @@ const MentorPage = ({ }) => {
               <Button startIcon={<GroupIcon />}>
                 تیم‌ها
               </Button>
-              <Button>درخواست‌ها</Button>
+              <Button>
+                <Badge badgeContent={2/*mentorRequestNo* todo*/} color="secondary">
+                  درخواست‌ها
+                </Badge>
+              </Button>
             </ButtonGroup>
           </Grid>
           <Hidden xsDown>
@@ -71,22 +84,72 @@ const MentorPage = ({ }) => {
 
         <Grid item sm={9} xs={12} justify='center'>
           <Paper elevation={3} classNames={classes.rightBox}>
-            
+            <Container className={classes.container}>
+              <Grid container item spacing={2} direction='row' justify='flex-start'>
+                <Grid item xs={12} sm={4}>
+                  <WorkShpoCard
+                    name={'هوش مصنوعی'}
+                    description={'این کارگاه خیلی قشنگ است. انگشت‌های خود را هم با آن می‌خورید :/'}
+                    teamNo={3}
+                    mentorNo={6}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <WorkShpoCard
+                    name={'هوش مصنوعی'}
+                    description={'این کارگاه خیلی قشنگ است. انگشت‌های خود را هم با آن می‌خورید :/'}
+                    teamNo={3}
+                    mentorNo={6}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <WorkShpoCard
+                    name={'هوش مصنوعی'}
+                    description={'این کارگاه خیلی قشنگ است. انگشت‌های خود را هم با آن می‌خورید :/'}
+                    teamNo={3}
+                    mentorNo={6}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <WorkShpoCard
+                    name={'هوش مصنوعی'}
+                    description={'این کارگاه خیلی قشنگ است. انگشت‌های خود را هم با آن می‌خورید :/'}
+                    teamNo={3}
+                    mentorNo={6}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <WorkShpoCard
+                    name={'هوش مصنوعی'}
+                    description={'این کارگاه خیلی قشنگ است. انگشت‌های خود را هم با آن می‌خورید :/'}
+                    teamNo={3}
+                    mentorNo={6}
+                  />
+                </Grid>
+
+                <Tooltip arrow title={'اضافه کردن کارگاه جدید'} className={classes.absolute}>
+                  <IconButton aria-label="close" /* onClick={handelClose} todo*/>
+                    <AddCircleIcon fontSize='large' />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+            </Container>
           </Paper>
         </Grid>
-
         <Hidden smUp>
           <Grid item fullWidth>
             <BackButton />
           </Grid>
         </Hidden>
       </Grid>
-    </Container>
+      <Backdrop className={classes.backdrop} open={isLoading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </Container >
   )
 }
 
 const mapStateToProps = (state) => {
-
 }
 
 export default connect(
