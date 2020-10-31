c9a2d5ddb4f47bcd51da75218c56af00ba2afd26
ab.reza <a.re.morteza@gmail.com>
2020-10-31 08:21:27 +0330

    fix getLocal issue

--- src/utils/getLocale.js
+++ src/utils/getLocale.js
@@ -1,4 +1,8 @@
 export default () => {
-  const Intl = JSON.parse(localStorage.getItem('reduxState')).Intl;
-  return Intl ? Intl.locale : 'fa';
+  try {
+    const Intl = JSON.parse(localStorage.getItem('reduxState')).Intl;
+    return Intl ? Intl.locale : 'fa';
+  } catch {
+    return 'fa';
+  }
 };
