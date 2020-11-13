27e209f9fdd2e7386746f1a6e6e0bb5a96c59b70
ab.reza <a.re.morteza@gmail.com>
2020-11-13 18:32:58 +0330

    pretty some code

--- src/redux/reducers/mentor.js
+++ src/redux/reducers/mentor.js
@@ -5,56 +5,51 @@ const initState = { allWorkshops: [] };
 function mentor(state = initState, action) {
   switch (action.type) {
     case actionTypes.WORKSHOP_TEAMS_REQUEST:
-      return ({
+      return {
         isFetching: true,
-      })
+      };
 
     case actionTypes.WORKSHOP_TEAMS_SUCCESS:
-      return ({
+      return {
         isFetching: false,
         teams: action.response.teams,
-      })
+      };
 
     case actionTypes.LOGIN_FAILURE:
-      return ({
+      return {
         isFetching: false,
-      })
-
-    ////////////////////////
+      };
 
     case actionTypes.ALL_WORKSHOPS_REQUEST:
-      return ({
+      return {
         isFetching: true,
-      })
+      };
 
     case actionTypes.ALL_WORKSHOPS_SUCCESS:
-      return ({
+      return {
         isFetching: false,
         allWorkshops: action.response.allWorkshops,
-      })
+      };
 
     case actionTypes.ALL_WORKSHOPS_FAILURE:
-      return ({
+      return {
         isFetching: false,
-      })
-
-    ////////////////////////
+      };
 
     case actionTypes.VISIT_TEAM_REQUEST:
-      return ({
+      return {
         isFetching: true,
-      })
+      };
 
     case actionTypes.VISIT_TEAM_SUCCESS:
-      return ({
+      return {
         isFetching: false,
-      })
+      };
 
     case actionTypes.VISIT_TEAM_FAILURE:
-      return ({
+      return {
         isFetching: false,
-      })
-
+      };
 
     default:
       return state;
