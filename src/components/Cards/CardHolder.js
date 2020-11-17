4108acbdd93d6490b5b0a19e1fb79c1f4d8fc259
ab.reza <a.re.morteza@gmail.com>
2020-11-17 18:58:59 +0330

    style(root): pretty some code

--- src/components/Cards/CardHolder.js
+++ src/components/Cards/CardHolder.js
@@ -1,31 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Button,
-  Container,
-  makeStyles,
-  Grid,
-  Paper,
-  ButtonGroup,
-  CssBaseline,
-  Hidden,
-  Backdrop,
-  Badge,
-  IconButton,
-  Tooltip,
-  Tabs,
-  Tab,
-  Box,
-  Typography,
-} from '@material-ui/core';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import GroupIcon from '@material-ui/icons/Group';
-import ClassIcon from '@material-ui/icons/Class';
+import React, { useState } from 'react';
+import { makeStyles, Grid } from '@material-ui/core';
 import { connect } from 'react-redux';
 import WorkshopCard from './WorkshopCard';
 import TeamCard from './TeamCard';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
-import CreateIcon from '@material-ui/icons/Create';
 
 const useStyles = makeStyles((theme) => ({
   cardHolder: {
@@ -37,8 +14,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-const CardHolder = ({ type, allWorkshops, workshopTeams, }) => {
+const CardHolder = ({ type, allWorkshops, workshopTeams }) => {
   const [cards, setCards] = useState();
   const classes = useStyles();
 
@@ -48,90 +24,75 @@ const CardHolder = ({ type, allWorkshops, workshopTeams, }) => {
       item
       spacing={2}
       direction="row"
-      className={classes.cardHolder}
-    >
-      {
-        type === 'workshops' &&
+      className={classes.cardHolder}>
+      {type === 'workshops' &&
         allWorkshops.map((workshop) => {
-          return (
-            <WorkshopCard {...workshop} />
-          )
-        })
-      }
+          return <WorkshopCard {...workshop} />;
+        })}
 
       {type === 'teams' &&
         workshopTeams.map((team) => {
-          return (
-            <TeamCard {...team} />
-          )
-        })
-      }
-
+          return <TeamCard {...team} />;
+        })}
     </Grid>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => {
   const allWorkshops = [
     {
       name: 'هوش مصنوعی',
-      description: 'این کارگاه خیلی قشنگ است. انگشت‌های خود را هم با آن می‌خورید :/',
- 
+      description:
+        'این کارگاه خیلی قشنگ است. انگشت‌های خود را هم با آن می‌خورید :/',
     },
     {
       name: 'هوش مصنوعی',
-      description: 'این کارگاه خیلی قشنگ است. انگشت‌های خود را هم با آن می‌خورید :/',
+      description:
+        'این کارگاه خیلی قشنگ است. انگشت‌های خود را هم با آن می‌خورید :/',
       teamsNumber: 3,
       mentorsNumber: 6,
     },
     {
       name: 'هوش مصنوعی',
-      description: 'این کارگاه خیلی قشنگ است. انگشت‌های خود را هم با آن می‌خورید :/',
+      description:
+        'این کارگاه خیلی قشنگ است. انگشت‌های خود را هم با آن می‌خورید :/',
       teamsNumber: 3,
       mentorsNumber: 6,
     },
-  ]
+  ];
   const workshopTeams = [
     {
-      "player": {
-        "player_type": "PARTICIPANT",
-        "name": "",
-        "id": 2,
-        "score": 3
+      player: {
+        player_type: 'PARTICIPANT',
+        name: '',
+        id: 2,
+        score: 3,
       },
-      "current_state": {
-        "id": 2,
-        "name": "ass"
-      }
+      current_state: {
+        id: 2,
+        name: 'ass',
+      },
     },
     {
-      "player": {
-        "player_type": "TEAM",
-        "id": 4,
-        "uuid": "c70d41a8-8151-469d-acf2-f4e4fe5ba9a9",
-        "group_name": "test team",
-        "score": 5,
-        "team_members": [
-          "faf@gmal.com"
-        ]
+      player: {
+        player_type: 'TEAM',
+        id: 4,
+        uuid: 'c70d41a8-8151-469d-acf2-f4e4fe5ba9a9',
+        group_name: 'test team',
+        score: 5,
+        team_members: ['faf@gmal.com'],
       },
-      "current_state": {
-        "id": 2,
-        "name": "ass"
-      }
-    }
-  ]
-  const teamMembers = {}
-  return ({
-    allWorkshops,//: state.mentor.allWorkshops,
+      current_state: {
+        id: 2,
+        name: 'ass',
+      },
+    },
+  ];
+  const teamMembers = {};
+  return {
+    allWorkshops, //: state.mentor.allWorkshops,
     workshopTeams,
-  })
-}
+  };
+};
 
-
-
-export default connect(
-  mapStateToProps,
-  {
-
-  })(CardHolder);
+export default connect(mapStateToProps, {})(CardHolder);
--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,23 +1,14 @@
 import React, { useState } from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import {
-  Button,
-  Fab,
-  Grid,
-  makeStyles,
-  Toolbar,
-  Typography,
-  Paper,
-} from '@material-ui/core';
-import Footer from '../components/Footer/Footer'
+import { Button, Fab, Grid, makeStyles, Typography } from '@material-ui/core';
+import Footer from '../components/Footer/Footer';
 import ScrollTop from '../components/ScrollToTop/ScrollToTop';
 import { KeyboardArrowUp as KeyboardArrowUpIcon } from '@material-ui/icons';
 import AuthDialog from '../components/Dialog/AuthDialog/AuthDialog';
-import CustomizedTimeline from '../components/TimeLine/TimeLine'
+import CustomizedTimeline from '../components/TimeLine/TimeLine';
 import FAQ from '../components/FAQ/FAQ';
-import ResponsiveAppBar from '../components/Appbar/ResponsiveAppBar';
-
+import { logout } from '../redux/actions/account';
+import { connect } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
   centerItems: {
@@ -58,7 +49,6 @@ const useStyles = makeStyles((theme) => ({
     backgroundSize: 'contain',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center center',
-
   },
 
   teamWorkImage: {
@@ -76,8 +66,11 @@ const useStyles = makeStyles((theme) => ({
   section1: {
     height: '100vh',
     color: 'black',
-    paddingTop: '50px',
-    paddingBottom: '50px',
+    padding: theme.spacing(4, 3, 4),
+  },
+
+  section1Grid: {
+    height: '80vh',
   },
 
   section2: {
@@ -105,93 +98,114 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: '30px',
   },
 
-
   firstPageImage: {
     width: '100%',
     // [theme.breakpoints.down('xs')]: {
     //   display: 'none',
     // },
   },
+  appbar: {
+    padding: theme.spacing(1, 0),
+  },
 }));
 
-export default function Homepage() {
+function Homepage({ isLoggedIn, logout }) {
   const classes = useStyles();
   const [authDialogOpen, setAuthDialogOpen] = useState();
 
   return (
     <>
-      {/* <ResponsiveAppBar /> */}
-      <Container className={`${classes.section1} ${classes.centerItems}`} >
-        <CssBaseline />
-        <Toolbar id="back-to-top-anchor" />
-        <div className='landing-background' />
-        <Grid
-          container
-          spacing={2}
-          justify='center'
-          direction='row'
-          alignItems='center'>
+      <Container className={classes.section1}>
+        <div id="back-to-top-anchor"></div>
+        <div className="landing-background" />
 
-          <Grid item xs={12} sm={8} md={6} justify='center' />
-
-          <Grid
-            container
-            item
-            xs={12}
-            sm={8}
-            md={6}
-            justify="center"
-            alignItems="center"
-            direction="column"
-            spacing={4}>
-
-            <Grid item>
-              <Typography
-                component="h1"
-                variant="h1"
-                className={classes.title}>
-                A-Lympiad
-              </Typography>
-              <Typography
-                component="h2"
-                variant="h3"
-                className={classes.subtitle}>
-                چهاردمین دوره مسابقات
-              </Typography>
+        <Grid container justify="space-between" direction="column">
+          <Grid item xs={12}>
+            <Grid container spacing={2}>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => setAuthDialogOpen(true)}>
+                  ورود به رویداد
+                </Button>
+              </Grid>
+              {isLoggedIn && (
+                <Grid item>
+                  <Button variant="outlined" onClick={() => logout()}>
+                    خروج
+                  </Button>
+                </Grid>
+              )}
             </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Grid container alignItems="center" direction="row">
+              <Grid item xs={12} sm={6} md={6} />
+              <Grid item xs={12} sm={6} md={6}>
+                <Grid
+                  container
+                  justify="center"
+                  alignItems="center"
+                  direction="column"
+                  className={classes.section1Grid}
+                  spacing={4}>
+                  <Grid item>
+                    <Typography
+                      component="h1"
+                      variant="h1"
+                      className={classes.title}>
+                      A-Lympiad
+                    </Typography>
+                    <Typography
+                      component="h2"
+                      variant="h3"
+                      className={classes.subtitle}>
+                      چهاردمین دوره مسابقات
+                    </Typography>
+                  </Grid>
 
-            <Grid item>
-              <Button
-                variant="contained"
-                color="primary"
-                size="large"
-                onClick={() => setAuthDialogOpen(true)}>
-                <Typography component="span" variant="h3">
-                  بزن بریم!
-                </Typography>
-              </Button>
+                  <Grid item>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      size="large"
+                      onClick={() => setAuthDialogOpen(true)}>
+                      <Typography component="span" variant="h3">
+                        بزن بریم!
+                      </Typography>
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
       </Container>
 
-      <Container className={`${classes.section2} ${classes.centerItems}`} >
-        <Grid container direction='column' spacing={2}>
+      <Container className={`${classes.section2} ${classes.centerItems}`}>
+        <Grid container direction="column" spacing={2}>
           <Grid item>
-            <Typography
-              component="h2"
-              variant="h2">
+            <Typography component="h2" variant="h2">
               ای‌لیمپیاد؟
             </Typography>
           </Grid>
-          <Grid container item direction='row'>
-            <Grid container item direciton='column' xs={12} sm={6} spacing={2}>
+          <Grid container item direction="row">
+            <Grid container item direciton="column" xs={12} sm={6} spacing={2}>
               <Grid item xs={12}>
                 <Typography
                   component="h3"
                   variant="h5"
                   className={classes.text}>
-                  امروزه مدل‌سازی ریاضی به عنوان ابزاری قوی در رشته‌های مختلف مورد استفاده قرار می‌گیرد. به همین منظور آموزش این شاخه از ریاضیات به دانش‌آموزان در دستور کار بسیاری از نظام‌های آموزشی قرار گرفته است. هرساله چندین مسابقه مدل‌سازی ریاضی در سراسر دنیا برای گروه‌های سنی مختلف برگزار می‌شود. یکی از این مسابقات، A-لیمپیاد است که با هدف افزایش قدرت تفکر، یادگیری تکنیک‌های مدل‌سازی ریاضی، کارگروهی، تمرین نوشتن یافته‌های علمی و جمع‌بندی و ارائه مطالب، در هلند و تعدادی دیگر از کشورها برگزار می‌شود.
+                  امروزه مدل‌سازی ریاضی به عنوان ابزاری قوی در رشته‌های مختلف
+                  مورد استفاده قرار می‌گیرد. به همین منظور آموزش این شاخه از
+                  ریاضیات به دانش‌آموزان در دستور کار بسیاری از نظام‌های آموزشی
+                  قرار گرفته است. هرساله چندین مسابقه مدل‌سازی ریاضی در سراسر
+                  دنیا برای گروه‌های سنی مختلف برگزار می‌شود. یکی از این
+                  مسابقات، A-لیمپیاد است که با هدف افزایش قدرت تفکر، یادگیری
+                  تکنیک‌های مدل‌سازی ریاضی، کارگروهی، تمرین نوشتن یافته‌های علمی
+                  و جمع‌بندی و ارائه مطالب، در هلند و تعدادی دیگر از کشورها
+                  برگزار می‌شود.
                 </Typography>
               </Grid>
               <Grid item xs={12}>
@@ -199,29 +213,48 @@ export default function Homepage() {
                   component="h3"
                   variant="h5"
                   className={classes.text}>
-                  دانش‌آموزان در این مسابقه برای حل یک مسئله در زندگی واقعی تلاش می‌کنند و در حقیقت مساله به کمک تکنیک‌های مختلف و فرضیات خود دانش‌آموزان حل می‌شود. دانش‌آموزان بایستی صورت دقیق مسئله را تفسیر کنند، استراتژی مناسبی برای حل بیابند، نتایج به دست آورده را تحلیل کنند و نتایج نهایی را ارائه نمایند. نتیجه نهایی این مسابقه گزارشی نوشته شده است که بایستی به روشنی فرضیات، تحلیل‌ها و جمع‌بندی دانش‌آموزان را نشان دهد.
+                  دانش‌آموزان در این مسابقه برای حل یک مسئله در زندگی واقعی تلاش
+                  می‌کنند و در حقیقت مساله به کمک تکنیک‌های مختلف و فرضیات خود
+                  دانش‌آموزان حل می‌شود. دانش‌آموزان بایستی صورت دقیق مسئله را
+                  تفسیر کنند، استراتژی مناسبی برای حل بیابند، نتایج به دست آورده
+                  را تحلیل کنند و نتایج نهایی را ارائه نمایند. نتیجه نهایی این
+                  مسابقه گزارشی نوشته شده است که بایستی به روشنی فرضیات،
+                  تحلیل‌ها و جمع‌بندی دانش‌آموزان را نشان دهد.
                 </Typography>
               </Grid>
             </Grid>
-            <Grid item xs={12} sm={6} className={classes.statImage} ></Grid>
+            <Grid item xs={12} sm={6} className={classes.statImage}></Grid>
           </Grid>
-          <Grid container item direction='row'>
-            <Grid item xs={12} sm={6} className={classes.teamWorkImage} ></Grid>
-            <Grid container item direciton='column' xs={12} sm={6} spacing={2}>
+          <Grid container item direction="row">
+            <Grid item xs={12} sm={6} className={classes.teamWorkImage}></Grid>
+            <Grid container item direciton="column" xs={12} sm={6} spacing={2}>
               <Grid item xs={12}>
                 <Typography
                   component="h3"
                   variant="h5"
                   className={classes.text}>
-                  این آزمون در بیش از ۹۰ مدرسه در هلند و حدود ۱۷ کشور در دنیا با محوریت موسسه فرودنتال، زیرمجموعه دانشگاه اترخت هلند در حال برگزاری است و در پی پیمان همکاری این موسسه با خانه ریاضیات اصفهان، این آزمون از سال ۱۳۸۶ تا کنون در ایران برگزار می‌گردد. این آزمون ویژگی‌های خاصی دارد:                </Typography>
+                  این آزمون در بیش از ۹۰ مدرسه در هلند و حدود ۱۷ کشور در دنیا با
+                  محوریت موسسه فرودنتال، زیرمجموعه دانشگاه اترخت هلند در حال
+                  برگزاری است و در پی پیمان همکاری این موسسه با خانه ریاضیات
+                  اصفهان، این آزمون از سال ۱۳۸۶ تا کنون در ایران برگزار می‌گردد.
+                  این آزمون ویژگی‌های خاصی دارد:{' '}
+                </Typography>
               </Grid>
               <Grid item xs={12}>
                 <Typography
                   component="h3"
                   variant="h5"
                   className={classes.text}>
+                  <li>آزمون به صورت گروهی انجام می‌شود.</li>
+                </Typography>
+                <Typography
+                  component="h3"
+                  variant="h5"
+                  className={classes.text}>
                   <li>
-                    آزمون به صورت گروهی انجام می‌شود.
+                    تیپ سوالات، عموما مسایل واقعی از هر جایی نظیر سازمان‌ها،
+                    شرکت‌ها، مراکز پژوهشی و یا مشکلات عمومی یک جامعه است و
+                    معمولا این سوال واقعا در جایی مطرح شده است.
                   </li>
                 </Typography>
                 <Typography
@@ -229,7 +262,8 @@ export default function Homepage() {
                   variant="h5"
                   className={classes.text}>
                   <li>
-                    تیپ سوالات، عموما مسایل واقعی از هر جایی نظیر سازمان‌ها، شرکت‌ها، مراکز پژوهشی و یا مشکلات عمومی یک جامعه است و معمولا این سوال واقعا در جایی مطرح شده است.
+                    عموما برای ارایه حل برای مساله، یک مدل ریاضی ارائه می‌شود و
+                    با یک ایده خاص روی آن بحث می‌شود.
                   </li>
                 </Typography>
                 <Typography
@@ -237,7 +271,9 @@ export default function Homepage() {
                   variant="h5"
                   className={classes.text}>
                   <li>
-                    عموما برای ارایه حل برای مساله، یک مدل ریاضی ارائه می‌شود و با یک ایده خاص روی آن بحث می‌شود.
+                    ایده‌های ارایه شده می‌توانند بسیار متنوع باشند و ممکن است هر
+                    کدام مشکلاتی داشته باشند و هیچ‌کدام مساله را ۱۰۰ درصد حل
+                    نکنند.
                   </li>
                 </Typography>
                 <Typography
@@ -245,7 +281,8 @@ export default function Homepage() {
                   variant="h5"
                   className={classes.text}>
                   <li>
-                    ایده‌های ارایه شده می‌توانند بسیار متنوع باشند و ممکن است هر کدام مشکلاتی داشته باشند و هیچ‌کدام مساله را ۱۰۰ درصد حل نکنند.
+                    عموما صورت مساله‌ها طولانی هستند و فرض‌های زیادی دارند و
+                    ممکن است یک حل برای یک مساله، از همه فرض‌ها استفاده نکند.
                   </li>
                 </Typography>
                 <Typography
@@ -253,7 +290,9 @@ export default function Homepage() {
                   variant="h5"
                   className={classes.text}>
                   <li>
-                    عموما صورت مساله‌ها طولانی هستند و فرض‌های زیادی دارند و ممکن است یک حل برای یک مساله، از همه فرض‌ها استفاده نکند.
+                    حل مساله‌ها نیز کوتاه نیست و عموما بحث‌های تحلیلی نیاز دارد
+                    که از این باب، مهارت نوشتن -چه از نظر مهارت چیدمان مطالب و
+                    چه از نظر نحوه استدلال- بسیار اهمیت داده می‌شود.{' '}
                   </li>
                 </Typography>
                 <Typography
@@ -261,14 +300,9 @@ export default function Homepage() {
                   variant="h5"
                   className={classes.text}>
                   <li>
-                    حل مساله‌ها نیز کوتاه نیست و عموما بحث‌های تحلیلی نیاز دارد که از این باب، مهارت نوشتن -چه از نظر مهارت چیدمان مطالب و چه از نظر نحوه استدلال- بسیار اهمیت داده می‌شود.                    </li>
-                </Typography>
-                <Typography
-                  component="h3"
-                  variant="h5"
-                  className={classes.text}>
-                  <li>
-                    بعضی از مسایل صرفا مهارت خواندن موثر فرض‌های مساله و سپس هم‌گردانی، استنتاج و تحلیل آن‌ها و ارایه یک خروجی تحلیل شده را خواسته است.
+                    بعضی از مسایل صرفا مهارت خواندن موثر فرض‌های مساله و سپس
+                    هم‌گردانی، استنتاج و تحلیل آن‌ها و ارایه یک خروجی تحلیل شده
+                    را خواسته است.
                   </li>
                 </Typography>
               </Grid>
@@ -277,12 +311,10 @@ export default function Homepage() {
         </Grid>
       </Container>
 
-      <Container className={`${classes.section3} ${classes.centerItems}`} >
-        <Grid container direction='column'>
+      <Container className={`${classes.section3} ${classes.centerItems}`}>
+        <Grid container direction="column">
           <Grid item>
-            <Typography
-              component="h2"
-              variant="h2">
+            <Typography component="h2" variant="h2">
               زمان‌بندی
             </Typography>
           </Grid>
@@ -292,13 +324,10 @@ export default function Homepage() {
         </Grid>
       </Container>
 
-
-      <Container className={`${classes.section4} ${classes.centerItems}`} >
-        <Grid container direction='column' spacing={2}>
+      <Container className={`${classes.section4} ${classes.centerItems}`}>
+        <Grid container direction="column" spacing={2}>
           <Grid item>
-            <Typography
-              component="h2"
-              variant="h2">
+            <Typography component="h2" variant="h2">
               سوالات رایج
             </Typography>
           </Grid>
@@ -317,8 +346,14 @@ export default function Homepage() {
       <Footer />
       <AuthDialog
         open={authDialogOpen}
-        handelClose={() => setAuthDialogOpen(false)}
+        handleClose={() => setAuthDialogOpen(false)}
       />
     </>
   );
 }
+
+const mapStateToProps = (state) => ({
+  isLoggedIn: !!state.account.token,
+});
+
+export default connect(mapStateToProps, { logout })(Homepage);
