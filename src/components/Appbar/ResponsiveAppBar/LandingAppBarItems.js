676bbc2f31de7ea7e2cc7faac3f483afa4a4ca8f
ab.reza <a.re.morteza@gmail.com>
2020-11-19 10:43:33 +0330

    fix remove sampleAuth issue

--- src/components/Appbar/ResponsiveAppBar/LandingAppBarItems.js
+++ src/components/Appbar/ResponsiveAppBar/LandingAppBarItems.js
@@ -1,18 +1,12 @@
-import { ListItem } from '@material-ui/core';
 import React from 'react';
-import AuthItem from './components/AuthItem';
 import LogoButton from './components/LogoButton';
 
 const LandingAppBarItems = () => ({
-  desktopLeftItems: [<AuthItem />],
+  desktopLeftItems: [],
   desktopRightItems: [<LogoButton />],
   mobileLeftItems: [<LogoButton />],
   mobileRightItems: [],
-  mobileMenuListItems: [
-    <ListItem>
-      <AuthItem />
-    </ListItem>,
-  ],
+  mobileMenuListItems: [],
 });
 
 export default LandingAppBarItems;
