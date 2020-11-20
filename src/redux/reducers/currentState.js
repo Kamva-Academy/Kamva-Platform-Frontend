59dc0360e78974f215b2a9687be4fc8cf5bb3e98
ab.reza <a.re.morteza@gmail.com>
2020-11-20 05:58:53 +0330

    fix SEND_ANSWER_SUCCESS redux

--- src/redux/reducers/currentState.js
+++ src/redux/reducers/currentState.js
@@ -13,6 +13,19 @@ function currentState(state = initState, action) {
         state: action.response,
       };
 
+    case actionTypes.SEND_ANSWER_SUCCESS:
+      return {
+        ...state,
+        state: {
+          ...state.state,
+          widgets: state.state.widgets.map((widget) =>
+            +widget.id === +action.response.problem
+              ? { ...widget, last_submit: action.response.xanswer }
+              : widget
+          ),
+        },
+      };
+
     case actionTypes.START_WORKSHOP_SUCCESS:
       if (action.response.error) {
         return state;
