e04295a4029b48ff7370167f6ce858380db550f9
ab.reza <a.re.morteza@gmail.com>
2020-12-11 20:09:29 +0330

    fix esilnt issue

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,37 +1,34 @@
-import {
-  Grid,
-} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import React from 'react';
 
-import InstitutionCard from './InstitutionCard'
+import InstitutionCard from './InstitutionCard';
 import Institutions from './Institutions';
 
 const Footer = () => {
   return (
     <Grid
       container
-      direction='column'
-      justify='center'
-      alignItems='center'
-      spacing={2}
-    >
-      <Grid container item direction='row' justify='center' sm={6}>
-        {Institutions.map((institution) => {
+      direction="column"
+      justify="center"
+      alignItems="center"
+      spacing={2}>
+      <Grid container item direction="row" justify="center" sm={6}>
+        {Institutions.map((institution, index) => {
           return (
             <InstitutionCard
+              key={index}
               img_src={institution.img_src}
               name={institution.name}
               site_url={institution.site_url}
             />
-          )
+          );
         })}
       </Grid>
-      <Grid item xs={12} justify='center' alignItems='center'>
+      <Grid item xs={12} justify="center" alignItems="center">
         «به راه بادیه رفتن به از نشستن باطل / و گر مراد نیابم به قدر وسع بکوشم»
       </Grid>
     </Grid>
   );
-}
-
+};
 
 export default Footer;
