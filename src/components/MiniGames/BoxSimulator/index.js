9033d1152b32d6ae155abaa1989c3ffc14971fc2
ab.reza <a.re.morteza@gmail.com>
2020-12-25 13:32:08 +0330

    init box simulator

---
+++ src/components/MiniGames/BoxSimulator/index.js
@@ -0,0 +1,17 @@
+import { Grid, Paper } from '@material-ui/core';
+import React from 'react';
+
+function BoxSimulator() {
+  return (
+    <Grid container direction="column" spacing={3}>
+      <Grid item>
+        <Paper>
+            
+        </Paper>
+      </Grid>
+      <Grid item></Grid>
+    </Grid>
+  );
+}
+
+export default BoxSimulator;
