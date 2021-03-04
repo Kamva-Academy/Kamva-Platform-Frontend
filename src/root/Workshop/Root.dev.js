13af10a189e375153a3d13c9942117eb4ebadf20
ab.reza <a.re.morteza@gmail.com>
2021-03-05 02:13:49 +0330

    remove unused import

--- src/root/Workshop/Root.dev.js
+++ src/root/Workshop/Root.dev.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import MiniGames from '../../components/MiniGames';
 import AboutUs from '../../containers/AboutUs';
--- src/root/ZeroJourneyer/Root.prod.js
+++ src/root/ZeroJourneyer/Root.prod.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import ZeroJourneyer from '../../containers/Landings/ZeroJourneyer';
 
