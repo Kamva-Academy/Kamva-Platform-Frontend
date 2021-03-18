b36c5beeba18f2db719e761423b6ea4ec37bb8ad
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-03-18 22:25:20 +0330

    fix roots

--- src/root/ZeroJourneyer/Root.prod.js
+++ src/root/ZeroJourneyer/Root.prod.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import MiniGames from '../../components/MiniGames';
 import Article from '../../containers/Article';
