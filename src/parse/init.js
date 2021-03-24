daec3719134439844720d750c871cb90ad8911fa
ab.reza <a.re.morteza@gmail.com>
2021-03-24 23:36:24 +0430

    change parse server url

--- src/parse/init.js
+++ src/parse/init.js
@@ -4,12 +4,12 @@ export const initParseServer = () => {
   let PARSE_SERVER_URL =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost/parse_server'
-      : 'https://zero.rastaiha.ir/parse_server';
+      : 'https://parse.rastaiha.ir/parse_server';
 
   let liveQueryServerURL =
     process.env.NODE_ENV === 'development'
       ? 'ws://localhost/ws/'
-      : 'wss://zero.rastaiha.ir/ws/';
+      : 'wss://parse.rastaiha.ir/ws/';
   Parse.initialize('asdfEWFkej2l3kj2lfjasfjasdf9', 'AKjdfkebfj323k238s9dfsdf');
   Parse.serverURL = PARSE_SERVER_URL;
   Parse.liveQueryServerURL = liveQueryServerURL;
