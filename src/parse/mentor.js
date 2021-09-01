e4ff972ceff488bd5f3323295c626f7db925afb9
Morteza Abolghasemi <a.re.morteza@gmail.com>
2021-09-01 11:26:04 +0430

    dont create request if exist

--- src/parse/mentor.js
+++ src/parse/mentor.js
@@ -2,7 +2,16 @@ import Parse from 'parse';
 
 const RequestMentor = Parse.Object.extend('RequestMentor');
 
+export const haveRequest = async({ teamId, fsmId }) =>{
+  const query = new Parse.Query('RequestMentor');
+  query.equalTo('teamId', teamId);
+  query.equalTo('fsmId', fsmId);
+  const requests = await query.find();
+  return requests.length > 0
+}
+
 export const requestMentor = async ({ playerId, teamId, fsmId }) => {
+  if(await haveRequest({ teamId, fsmId })) return;
   const requestMentor = new RequestMentor();
   requestMentor.set('playerId', playerId);
   requestMentor.set('teamId', teamId);
