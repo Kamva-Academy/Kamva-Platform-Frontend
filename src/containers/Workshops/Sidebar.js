20bb18aef2be65216278cd829ce95f632d6b7f5d
Danny Khorasanizadeh <dannykhorasanizadeh@ec.iut.ac.ir>
2021-12-20 22:51:04 +0330

    Add donate section

--- src/containers/Workshops/Sidebar.js
+++ src/containers/Workshops/Sidebar.js
@@ -10,62 +10,150 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
 import {
   getCertificateAction,
   getOneEventInfoAction,
   getOneRegistrationFormAction,
 } from '../../redux/slices/events';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginBottom: theme.spacing(3),
+  },
+  formControl: {
+    width: '100%',
+  },
+  customAmountBox: {
+    display: 'none',
+  },
 }));
 
 function Workshops({
   getCertificate,
   getOneEventInfo,
   getOneRegistrationForm,
-
   registrationForm,
   event,
 }) {
   const classes = useStyles();
   const { eventId } = useParams();
+  const [amount, setAmount] = React.useState('');
 
   useEffect(() => {
     getOneEventInfo({ id: eventId });
   }, [getOneEventInfo]);
 
+  const handleAmountChange = (event) => {
+    const box = document.getElementById('customAmount');
+    if (event.target.value == 'Custom') {
+      box.style.display = 'unset';
+      setAmount(50000);
+    } else {
+      box.style.display = 'none';
+      setAmount(event.target.value);
+    }
+  };
+  const handleTextboxAmountChange = (event) => {
+    setAmount(parseInt(event.target.value));
+  };
   const doGetCertificate = () => {
-    getCertificate({ registrationReceiptId: event?.registration_receipt }).then((action) => {
-      if (action.meta.requestStatus == 'fulfilled') {
-        window.open(action.payload.response.certificate, '_blank');
+    getCertificate({ registrationReceiptId: event?.registration_receipt }).then(
+      (action) => {
+        if (action.meta.requestStatus == 'fulfilled') {
+          window.open(action.payload.response.certificate, '_blank');
+        }
       }
-    });
-  }
-
+    );
+  };
+  const payDonation = () => {
+    console.log(amount);
+  };
   return (
-    <Grid container spacing={2} alignItems='center' component={Paper}>
-      <Grid item xs={12}>
-        <Typography variant='h4'>
-          {`به رویداد ${event?.name || ''} خوش آمدید!`}
-        </Typography>
+    <>
+      <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        component={Paper}
+        className={classes.mainGrid}>
+        <Grid item xs={12}>
+          <Typography variant="h4">
+            {`به رویداد ${event?.name || ''} خوش آمدید!`}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            disabled={event?.event_type == 'Individual'}
+            variant="outlined"
+            fullWidth
+            component={Link}
+            to={`/event/${event?.id}/team_selection/`}>
+            {'تیم‌کشی'}
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            disabled={!event?.has_certificate || !event?.certificates_ready}
+            onClick={doGetCertificate}
+            variant="outlined"
+            fullWidth>
+            {'گواهی حضور'}
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Button
-          disabled={event?.event_type == 'Individual'}
-          variant="outlined"
-          fullWidth
-          component={Link}
-          to={`/event/${event?.id}/team_selection/`}>
-          {'تیم‌کشی'}
-        </Button>
+      <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        component={Paper}
+        className={classes.mainGrid}>
+        <Grid item xs={12}>
+          <Typography variant="h4">{'کمک مالی'}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl variant="filled" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-filled-label">مقدار</InputLabel>
+            <Select
+              labelId="demo-simple-select-filled-label"
+              id="demo-simple-select-filled"
+              value={amount}
+              onChange={handleAmountChange}>
+              <MenuItem value={10000}>ده هزار تومان</MenuItem>
+              <MenuItem value={20000}>بیست هزار تومان</MenuItem>
+              <MenuItem value={30000}>سی هزار تومان</MenuItem>
+              <MenuItem value={'Custom'}>مقدار دیگر</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <form
+            className={classes.customAmountBox}
+            noValidate
+            autoComplete="off"
+            fullWidth
+            id={'customAmount'}>
+            <TextField
+              id="standard-basic"
+              fullWidth
+              value={amount}
+              type="number"
+              onChange={handleTextboxAmountChange}
+            />
+            <Typography variant="p">{'تومان'}</Typography>
+          </form>
+        </Grid>
+        <Grid item xs={12}>
+          <Button onClick={payDonation} variant="outlined" fullWidth>
+            {'پرداخت'}
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Button disabled={!event?.has_certificate || !event?.certificates_ready} onClick={doGetCertificate} variant='outlined' fullWidth>
-          {'گواهی حضور'}
-        </Button>
-      </Grid>
-    </Grid>
+    </>
   );
 }
 
@@ -74,11 +162,8 @@ const mapStateToProps = (state) => ({
   registrationForm: state.events.registrationForm,
 });
 
-export default connect(
-  mapStateToProps,
-  {
-    getCertificate: getCertificateAction,
-    getOneEventInfo: getOneEventInfoAction,
-    getOneRegistrationForm: getOneRegistrationFormAction,
-  }
-)(Workshops);
+export default connect(mapStateToProps, {
+  getCertificate: getCertificateAction,
+  getOneEventInfo: getOneEventInfoAction,
+  getOneRegistrationForm: getOneRegistrationFormAction,
+})(Workshops);
