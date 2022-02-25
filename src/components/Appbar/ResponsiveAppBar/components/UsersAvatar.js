eca7b9bec31bf33c03dc2a13825208a3fa34f1a7
amoohashem <amoo.hashem.mehraban@gmail.com>
2022-02-25 06:46:10 +0330

    show team name

--- src/components/Appbar/ResponsiveAppBar/components/UsersAvatar.js
+++ src/components/Appbar/ResponsiveAppBar/components/UsersAvatar.js
@@ -1,5 +1,13 @@
-import { Avatar, Badge, Tooltip } from '@material-ui/core';
-import { OfflineBolt } from '@material-ui/icons';
+import {
+  Avatar,
+  Badge,
+  Tooltip,
+  Menu,
+  MenuItem,
+  Typography,
+  Box,
+} from '@material-ui/core';
+import { MapTwoTone, OfflineBolt } from '@material-ui/icons';
 import { AvatarGroup } from '@material-ui/lab';
 import React, { useContext } from 'react';
 
@@ -9,25 +17,55 @@ import { stringToColor } from '../../../../utils/stringToColor';
 const UsersAvatar = () => {
   const { myTeam } = useContext(StatePageContext);
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  console.log(myTeam)
+
   return (
-    <AvatarGroup max={4}>
-      {myTeam?.members?.map((member) => (
-        <Tooltip
-          key={member.id}
-          arrow
-          title={
-            `${member.first_name} ${member.last_name}` +
-            ((myTeam?.team_head === member.id && myTeam?.members?.length != 1) ? ' (سرگروه) ' : '')
-          }>
-          <Badge
-            overlap="circle"
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            invisible={myTeam?.team_head !== member.id}
-            badgeContent={<OfflineBolt style={{ color: 'gold' }} />}>
-            <div>
+    <>
+      <Tooltip
+        onClick={handleClick}
+        arrow
+        title={`تیم ${myTeam?.name}`}>
+        <Avatar
+          style={{
+            backgroundColor: stringToColor(myTeam?.name),
+            border: '0.1px solid lightgray',
+          }}>
+          {myTeam?.name[0]}
+        </Avatar>
+      </Tooltip>
+
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem>
+          {`تیم ${myTeam?.name}`}
+        </MenuItem>
+
+        {myTeam?.members?.map((member) =>
+          <MenuItem key={member.id}>
+            <Badge
+              key={member.id}
+              overlap="circle"
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              invisible={myTeam?.team_head !== member.id}
+              badgeContent={<OfflineBolt style={{ color: 'gold' }} />}>
               <Avatar
                 style={{
                   backgroundColor: stringToColor(
@@ -37,11 +75,15 @@ const UsersAvatar = () => {
                 }}>
                 {`${member.first_name[0]}`}
               </Avatar>
-            </div>
-          </Badge>
-        </Tooltip>
-      ))}
-    </AvatarGroup>
+            </Badge>
+            <Box ml={1}>
+              {`${member?.first_name} ${member?.last_name}`}
+            </Box>
+          </MenuItem>
+
+        )}
+      </Menu>
+    </>
   );
 };
 
