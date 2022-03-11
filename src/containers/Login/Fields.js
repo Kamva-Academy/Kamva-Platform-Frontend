f1378ce97ca5d62de5eac596598057446d0329fa
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-03-11 19:55:00 +0330

    hotfix : disable login button

--- src/containers/Login/Fields.js
+++ src/containers/Login/Fields.js
@@ -87,11 +87,15 @@ const InputFields = ({ isFetching, login, addNotification, token }) => {
       </Grid>
 
       <Grid container item direction="row" justifyContent="center">
+        <Typography variant='h4' color='error' align='center'>
+          {'سایت در حال تعمیر است. فعلا نمی‌توانید وارد حساب کاربری خود شوید!'}
+        </Typography>
         <Button
+          disabled
           onClick={doLogin}
           variant="contained"
           color="primary"
-          disabled={isFetching}
+          // disabled={isFetching}
           fullWidth>
           بزن بریم
         </Button>
