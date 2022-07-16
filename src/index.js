6e42925c07c5212dc6e0542db21c8e9de0c1962d
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-07-16 23:46:14 +0430

    add sentry

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from './App';
 import reduxStore from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+Sentry.init({
+  dsn: "https://1101899554bf45c48a4cd2e6a7e26ac2@sentry.hamravesh.com/368",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={reduxStore}>
       <App />
