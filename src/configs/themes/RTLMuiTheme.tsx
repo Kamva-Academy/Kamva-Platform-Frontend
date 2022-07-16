142dcdfdbf47fe3ddc6c6f71454960c1e269fd97
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-07-17 00:53:12 +0430

    load font by mui

--- src/configs/themes/RTLMuiTheme.tsx
+++ src/configs/themes/RTLMuiTheme.tsx
@@ -2,11 +2,24 @@ import { createTheme } from '@mui/material';
 import { Workshop } from './MuiVariables';
 import theme from './theme';
 import typography from './typography';
+import iranyekanweblight from '../fonts/iranyekanweblight.woff';
+import iranyekanweblight2 from '../fonts/iranyekanweblight.woff2';
 
 const RTLMuiTheme = createTheme({
   direction: 'rtl',
   ...theme(Workshop),
-  typography
+  typography,
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+      @font-face {
+        font-family: iranyekan;
+        src: url(${iranyekanweblight}) format("woff"), url(${iranyekanweblight2}) format("woff2");
+        font-weight: 100 900;
+      }
+      `,
+    },
+  },
 });
 
 export default RTLMuiTheme;
