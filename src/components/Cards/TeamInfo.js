8f7daadc784e42de8da84e49b7d89ef792bf494e
AlipourIm <iman.alip2001@gmail.com>
2022-08-16 13:31:23 +0430

    add text field for team link

--- src/components/Cards/TeamInfo.js
+++ src/components/Cards/TeamInfo.js
@@ -8,12 +8,13 @@ import {
   FormControlLabel,
   Grid,
   Typography,
-  Box
+  TextField,
 } from '@mui/material';
 import { NotificationsActive } from '@mui/icons-material';
 import { makeStyles } from '@mui/styles'
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { connect } from 'react-redux';
+import validateURL from '../../utils/validators/urlValidator'
 
 import {
   makeTeamHeadAction,
@@ -35,34 +36,38 @@ const TeamInfo = ({
   members,
   teamId,
   playerId,
-
   makeTeamHead,
 }) => {
   const classes = useStyles();
+  const [teamLink, setTeamLink] = useState('')
+  const [linkIsNotValid, setLinkIsNotValid] = useState(false)
+
+  useEffect(() => {
+    setLinkIsNotValid(!validateURL(teamLink))
+  }, [teamLink])
 
   return (
-    <Card className={classes.root} 
-    sx={{
-      maxWidth: 300,
-      marginTop: '0px',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      height: '100%',
-      width: '100%',
-      padding: '0px !important',
-      backgroundColor: 'rgb(255, 255, 255, 0.94)',
-      fontSize: '1rem',
-      textDecoration: 'none',
-      overflow: 'hidden',
-      boxShadow: '0 0 1px 0rem rgba(0, 0, 0, 0.5)',
-      transition: 'transform 0.1s ease-in-out',
-      display: 'flex',
-      justifyContent: 'space-between',
-      flexDirection: 'column',
-      '&:hover': {
-        transform: 'translateY(-0.1rem) scale(1.01)',
-        boxShadow: '0 0.5em 1rem -1rem rgba(2, 2, 2, 2.5)',
-      },}}
+    <Card className={classes.root}
+      sx={{
+        maxWidth: 300,
+        margin: '0px auto',
+        height: '100%',
+        width: '100%',
+        padding: '0px !important',
+        backgroundColor: 'rgb(255, 255, 255, 0.94)',
+        fontSize: '1rem',
+        textDecoration: 'none',
+        overflow: 'hidden',
+        boxShadow: '0 0 1px 0rem rgba(0, 0, 0, 0.5)',
+        transition: 'transform 0.1s ease-in-out',
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexDirection: 'column',
+        '&:hover': {
+          transform: 'translateY(-0.1rem) scale(1.01)',
+          boxShadow: '0 0.5em 1rem -1rem rgba(2, 2, 2, 2.5)',
+        },
+      }}
 
     >
       <CardContent>
@@ -71,7 +76,7 @@ const TeamInfo = ({
           {name}
         </Typography>
         <Grid container spacing={1}>
-          {members.map((member) => (
+          {members.length > 0 ? members.map((member) => (
             <Grid container item key={member.id} alignItems='start' justifyContent='start'>
               <FormControlLabel
                 control={
@@ -82,19 +87,33 @@ const TeamInfo = ({
                     }}
                     color="primary" />
                 }
-                label={`${member?.first_name} ${member?.last_name}`}
+                label={`${member?.first_name} ${member?.last_name}` === ' ' ? 'بی‌نام!' : `${member?.first_name} ${member?.last_name}`}
                 labelPlacement="end"
               />
             </Grid>
 
-          ))}
+          ))
+            :
+            <Typography marginLeft='10px' marginTop='20px'>این تیم هیچ عضوی ندارد.</Typography>}
         </Grid>
       </CardContent>
       <CardActions>
         <Grid container direction="column" spacing={1}>
           <Grid item>
-            <ButtonGroup disabled variant="outlined" color="primary" fullWidth>
-              <Button>{'ویرایش'}</Button>
+            <TextField
+              error= {linkIsNotValid && true}
+              helperText= {linkIsNotValid && "ورودی وارد شده لینک معتبری نیست"}
+              id="standard-multiline-static"
+              label="لینک تیم"
+              multiline
+              rows={3}
+              placeholder="somelink.somedomain"
+              variant="outlined"
+              onChange={(e) => setTeamLink(e.target.value)}
+              sx={{marginBottom: '30px', width: '100%', direction: 'rtl'}}
+            />
+            <ButtonGroup sx={{ height: '40px' }} disabled variant="outlined" color="primary" fullWidth>
+              <Button>{'بروزرسانی لینک'}</Button>
               <Button>{'حذف'}</Button>
             </ButtonGroup>
           </Grid>
