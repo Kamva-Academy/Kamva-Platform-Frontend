713e03b43fb2c36fd24e8fca4d96a80606548c45
AlipourIm <iman.alip2001@gmail.com>
2022-08-21 15:29:40 +0430

    add star team feature

--- src/components/Cards/TeamWorkshopInfo.tsx
+++ src/components/Cards/TeamWorkshopInfo.tsx
@@ -13,11 +13,15 @@ import {
   Stack,
   Tooltip,
   Typography,
+  SvgIcon,
+  IconButton,
 } from '@mui/material';
 import { NotificationsActive } from '@mui/icons-material';
 import React, { FC, useEffect, useState, useRef } from 'react';
 import { connect } from 'react-redux';
 import AccessTimeIcon from '@mui/icons-material/AccessTime'
+import StarIcon from '@mui/icons-material/Star';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 import {
   deleteRequestMentorAction,
@@ -45,7 +49,9 @@ type TeamWorkshopInfoPropsType = {
   getPlayerFromTeam: Function,
   mentorsInRoom: Mentor[],
   startProblemTime: string,
-  teamStage: String
+  teamStage: String,
+  isStarred: Boolean,
+  toggleStar: Function
 }
 
 const TeamWorkshopInfo: FC<TeamWorkshopInfoPropsType> = ({
@@ -57,8 +63,9 @@ const TeamWorkshopInfo: FC<TeamWorkshopInfoPropsType> = ({
   playerIdFromRedux,
   deleteRequestMentor,
   getPlayerFromTeam,
+  isStarred,
+  toggleStar,
 }) => {
-  const navigate = useNavigate()
   const { eventId, fsmId } = useParams();
   const [click, setClick] = useState(false);
   const subscriberRef = useRef(null);
@@ -66,35 +73,38 @@ const TeamWorkshopInfo: FC<TeamWorkshopInfoPropsType> = ({
   const [teamEnterTimeToState, setTeamEnterTimeToState] = useState('')
   const [currentStateName, setCurrentStateName] = useState('')
   const [mentorsInRoom, setMentorsInRoom] = useState([]);
+  const [showStarAnimation, setShowStarAnimation] = useState(false)
 
-  useEffect(() => {
-    const subscribeOnMentorArrival = async () => {
-      const mentorsInRoom = await getMentorsInRoom(teamId)
-      await checkForOfflineMentors();
-      setMentorsInRoom(mentorsInRoom);
-      const subscriber = await getMentorsInRoomSubscription(teamId);
-      subscriber.on('create', async (newState) => {
-        if (newState.get('uuid') === teamId) {
-          setMentorsInRoom(await getMentorsInRoom(teamId));
-        }
-      });
-      subscriber.on('update', async (newState) => {
-        if (newState.get('uuid') === teamId) {
-          setMentorsInRoom(await getMentorsInRoom(teamId))
-        }
-      });
-      mentorsInRoomSubscriberRef.current = subscriber;
-    }
-    subscribeOnMentorArrival();
+  useEffect(() => setShowStarAnimation(false), [])
 
-    return (() => {
-      mentorsInRoomSubscriberRef.current?.unsubscribe();
-    })
-  }, [])
+  // useEffect(() => {
+  //   const subscribeOnMentorArrival = async () => {
+  //     const mentorsInRoom = await getMentorsInRoom(teamId)
+  //     await checkForOfflineMentors();
+  //     setMentorsInRoom(mentorsInRoom);
+  //     const subscriber = await getMentorsInRoomSubscription(teamId);
+  //     subscriber.on('create', async (newState) => {
+  //       if (newState.get('uuid') === teamId) {
+  //         setMentorsInRoom(await getMentorsInRoom(teamId));
+  //       }
+  //     });
+  //     subscriber.on('update', async (newState) => {
+  //       if (newState.get('uuid') === teamId) {
+  //         setMentorsInRoom(await getMentorsInRoom(teamId))
+  //       }
+  //     });
+  //     mentorsInRoomSubscriberRef.current = subscriber;
+  //   }
+  //   subscribeOnMentorArrival();
+
+  //   return (() => {
+  //     mentorsInRoomSubscriberRef.current?.unsubscribe();
+  //   })
+  // }, [])
 
   const checkForOfflineMentors = async () => {
-    for (let i = 0; i < mentorsInRoom.length; i++){
-      if (Math.abs(moment.duration(moment().diff(moment(mentorsInRoom[i].get('MentorLastUpdated'), 'HH:mm:ss'))).asMilliseconds()) > 20000){
+    for (let i = 0; i < mentorsInRoom.length; i++) {
+      if (Math.abs(moment.duration(moment().diff(moment(mentorsInRoom[i].get('MentorLastUpdated'), 'HH:mm:ss'))).asMilliseconds()) > 20000) {
         await announceMentorDeparture(mentorsInRoom[i].get('uuid'), mentorsInRoom[i].get('MentorId'))
       }
     }
@@ -113,37 +123,37 @@ const TeamWorkshopInfo: FC<TeamWorkshopInfoPropsType> = ({
     })
   }, [mentorsInRoom])
 
-  useEffect(() => {
-    const subscribeOnStateChange = async () => {
-      const state = await getTeamState(teamId);
-      if (!state) return;
-      setCurrentStateName(state.get('currentStateName'))
-      setTeamEnterTimeToState(state.get('teamEnterTimeToState'))
-      const subscriber = await getTeamStateSubscription();
-      subscriber.on('create', (newState) => {
-        if (newState.get('uuid') === teamId) {
-          const currentStageNameTmp = newState.get('currentStateName');
-          const teamEnterTimeToStateTmp = moment()
-          setCurrentStateName(currentStageNameTmp)
-          setTeamEnterTimeToState(teamEnterTimeToStateTmp)
-        }
-      });
-      subscriber.on('update', (newState) => {
-        if (newState.get('uuid') === teamId) {
-          const currentStageNameTmp = newState.get('currentStateName');
-          const teamEnterTimeToStateTmp = moment()
-          setCurrentStateName(currentStageNameTmp)
-          setTeamEnterTimeToState(teamEnterTimeToStateTmp)
-        }
-      });
-      subscriberRef.current = subscriber;
-    }
-    subscribeOnStateChange()
+  // useEffect(() => {
+  //   const subscribeOnStateChange = async () => {
+  //     const state = await getTeamState(teamId);
+  //     if (!state) return;
+  //     setCurrentStateName(state.get('currentStateName'))
+  //     setTeamEnterTimeToState(state.get('teamEnterTimeToState'))
+  //     const subscriber = await getTeamStateSubscription();
+  //     subscriber.on('create', (newState) => {
+  //       if (newState.get('uuid') === teamId) {
+  //         const currentStageNameTmp = newState.get('currentStateName');
+  //         const teamEnterTimeToStateTmp = moment()
+  //         setCurrentStateName(currentStageNameTmp)
+  //         setTeamEnterTimeToState(teamEnterTimeToStateTmp)
+  //       }
+  //     });
+  //     subscriber.on('update', (newState) => {
+  //       if (newState.get('uuid') === teamId) {
+  //         const currentStageNameTmp = newState.get('currentStateName');
+  //         const teamEnterTimeToStateTmp = moment()
+  //         setCurrentStateName(currentStageNameTmp)
+  //         setTeamEnterTimeToState(teamEnterTimeToStateTmp)
+  //       }
+  //     });
+  //     subscriberRef.current = subscriber;
+  //   }
+  //   subscribeOnStateChange()
 
-    return () => {
-      subscriberRef.current?.unsubscribe();
-    };
-  }, []);
+  //   return () => {
+  //     subscriberRef.current?.unsubscribe();
+  //   };
+  // }, []);
 
   {/* this function redirects mentor to a teams page, this team could have requested mentor or not, if so, we use the
 available playerId field, otherwise we fetch one team members Id and use it to access their page */}
@@ -157,7 +167,7 @@ available playerId field, otherwise we fetch one team members Id and use it to a
   useEffect(() => {
     if ((playerId || playerIdFromRedux) && click) {
       setClick(false);
-      window.open(`/event/${eventId}/workshop/${fsmId}?playerId=${playerId || playerIdFromRedux}`, '_blank');
+      window.open(`/event/${eventId}/workshop/${fsmId}?playerId=${playerId || playerIdFromRedux}&teamId=${teamId}`, '_blank');
     }
   }, [playerId, click, playerIdFromRedux])
 
@@ -198,7 +208,11 @@ available playerId field, otherwise we fetch one team members Id and use it to a
             alignItems: 'center'
           }}
         >
-          {playerId ? <NotificationsActive sx={{ animation: "bellRing 1.4s infinite", width: "40px" }} color="primary" /> : <Box />}
+          {playerId ?
+              <NotificationsActive sx={{ animation: "bellRing 1.4s infinite"}} color="primary" /> 
+              :
+              <SvgIcon sx={{color: 'gold', ...((showStarAnimation && isStarred) && {animation: "starred 0.9s 1"})}} onClick={() => {setShowStarAnimation(true);toggleStar(teamId);}} component={isStarred ? StarIcon : StarBorderIcon}/>
+          }
           {mentorsInRoom?.length > 0 ?
             <Stack direction="row" sx={{ justifyContent: 'start', alignItems: "center" }}>
               <Typography sx={{ fontSize: "8px", margin: '10px' }}>
@@ -233,7 +247,7 @@ available playerId field, otherwise we fetch one team members Id and use it to a
 
         <CardActionArea disabled> {/* this action holds each cards name and members */}
           <CardContent sx={{ paddingBottom: '0px' }}>
-            <Typography gutterBottom variant="h3">
+            <Typography marginLeft='5px' gutterBottom variant="h3">
               {name}
             </Typography>
             <Grid container direction="row" justifyContent="start">
