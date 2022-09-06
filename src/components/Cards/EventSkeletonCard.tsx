ea55abd63e8662b6285d240183b09216d40ed189
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-09-06 19:48:48 +0430

    add todo for Sadegh

--- src/components/Cards/EventSkeletonCard.tsx
+++ src/components/Cards/EventSkeletonCard.tsx
@@ -1,48 +1,48 @@
-import { Card, Chip, Grid, Typography, Skeleton } from '@mui/material';
-import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import { Grid, Skeleton } from '@mui/material';
 import React, { FC } from 'react';
 
 const EventSkeletonCard: FC = () => {
   return (
-      <Grid
-        container
-        alignItems='stretch'
+    <Grid
+      container
+      alignItems='stretch'
+      sx={(theme) => ({
+        // todo SADEGH
+        height: '100%',
+        justifyContent: 'space-between',
+        [theme.breakpoints.down('md')]: {
+          justifyContent: 'center',
+        },
+      })}>
+      <Grid sx={{ padding: 0 }} item container justifyContent="center" alignItems="center" xs={12} md={5}>
+        <Skeleton variant="rectangular" width="100%" height={310} />
+      </Grid>
+
+      <Grid item container xs={12} md={7}
+        direction="column"
+        justifyContent="space-between" spacing={2}
         sx={(theme) => ({
-          height: '100%',
-          justifyContent: 'space-between',
-          [theme.breakpoints.down('md')]: {
-            justifyContent: 'center',
-          },
+          padding: theme.spacing(2),
+          paddingLeft: theme.spacing(2),
         })}>
-        <Grid sx={{ padding: 0 }} item container justifyContent="center" alignItems="center" xs={12} md={5}>
-            <Skeleton variant="rectangular" width="100%" height={310} />
+        <Grid item>
+          <Skeleton width="50%" />
         </Grid>
-        
-        <Grid item container xs={12} md={7}
-          direction="column"
-          justifyContent="space-between" spacing={2}
-          sx={(theme) => ({
-            padding: theme.spacing(2),
-            paddingLeft: theme.spacing(2),
-          })}>
-          <Grid item>
-            <Skeleton width="50%" />
-          </Grid>
-          <Grid item>
-            <Skeleton width="80%" />
-            <Skeleton width="80%" />
-            <Skeleton width="80%" />
-            <Skeleton width="80%" />
-            <Skeleton width="80%" />
-          </Grid>
-          <Grid item>
-            <Skeleton width="40%" />
-          </Grid>
-          <Grid item>
-            <Skeleton width="80%" />
-          </Grid>
+        <Grid item>
+          <Skeleton width="80%" />
+          <Skeleton width="80%" />
+          <Skeleton width="80%" />
+          <Skeleton width="80%" />
+          <Skeleton width="80%" />
+        </Grid>
+        <Grid item>
+          <Skeleton width="40%" />
+        </Grid>
+        <Grid item>
+          <Skeleton width="80%" />
         </Grid>
       </Grid>
+    </Grid>
   );
 };
 
