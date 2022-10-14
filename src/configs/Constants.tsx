edf5e61e7ab74c625d6d9afd1a62b7290907c87f
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-10-14 20:34:52 +0330

    update new backend link

--- src/configs/Constants.tsx
+++ src/configs/Constants.tsx
@@ -1,6 +1,6 @@
 export const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'https://backend.rastaiha.ir'
-    : 'https://backend.rastaiha.ir';
+    ? 'https://kamva-backend.darkube.app'
+    : 'http://localhost:8000'
 
 export const ITEMS_PER_PAGE_NUMBER = 12;
