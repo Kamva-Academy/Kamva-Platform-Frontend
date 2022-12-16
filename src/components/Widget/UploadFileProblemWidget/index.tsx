c0788d7f570b45862e67404b9565f93bee627f5a
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-12-16 11:39:10 +0330

    minor improve in upload file widget

--- src/components/Widget/UploadFileProblemWidget/index.tsx
+++ src/components/Widget/UploadFileProblemWidget/index.tsx
@@ -13,6 +13,7 @@ import {
 import UploadFileProblemEditWidget from './edit';
 import { WidgetModes } from '..';
 import { BASE_URL } from '../../../configs/Constants';
+import { toast } from 'react-toastify';
 
 type UploadFileProblemWidgetPropsType = {
   collectAnswers: any;
@@ -39,6 +40,8 @@ const UploadFileProblemWidget: FC<UploadFileProblemWidgetPropsType> = ({
   const t = useTranslate();
   const [file, setFile] = useState(null);
 
+  console.log(props)
+
   useEffect(() => {
     if (last_submitted_answer) {
       setFile({
@@ -52,29 +55,35 @@ const UploadFileProblemWidget: FC<UploadFileProblemWidgetPropsType> = ({
     e.preventDefault();
     const file = e.target.files[0];
     if (file) {
-      if (file.size <= 8e6) {
-        uploadFileAnswer({
-          problemId: widgetId,
-          fileName: file.name,
-          answerFile: file,
-        }).then((response) => {
-          if (response.type?.endsWith('fulfilled')) {
-            setFile({
-              link: response.payload?.response?.answer_file,
-              name: file.name,
-            });
-            if (mode === WidgetModes.InAnswerSheet) {
-              collectAnswers('upload_file_answer', response.payload?.response?.id);
-            }
-          } else {
-            e.target.value = null;
-          }
-        })
-      } else {
+      if (file.name.length > 100) {
+        toast.error('حداکثر طول نام فایل حداکثر ۱۰۰ کاراکتر است.');
         e.target.value = null;
-        e.target.setCustomValidity('Maximum upload file size is 8 MB.');
         e.target.reportValidity();
+        return;
       }
+      if (file.size > 11e6) {
+        toast.error('حجم فایل ارسالی باید کمتر از ۱۰ مگابایت باشد.')
+        e.target.value = null;
+        e.target.reportValidity();
+        return;
+      }
+      uploadFileAnswer({
+        problemId: widgetId,
+        fileName: file.name,
+        answerFile: file,
+      }).then((response) => {
+        if (response.type?.endsWith('fulfilled')) {
+          setFile({
+            link: response.payload?.response?.answer_file,
+            name: file.name,
+          });
+          if (mode === WidgetModes.InAnswerSheet) {
+            collectAnswers('upload_file_answer', response.payload?.response?.id);
+          }
+        } else {
+          e.target.value = null;
+        }
+      })
     }
   };
 
@@ -108,7 +117,7 @@ const UploadFileProblemWidget: FC<UploadFileProblemWidgetPropsType> = ({
               {t('uploadFile')}
             </Button>
             <input
-              accept="application/pdf,image/*"
+              accept="application/pdf,image/*,.zip,.rar"
               style={{ display: 'none' }}
               id={'raised-button-file' + widgetId}
               type="file"
@@ -133,7 +142,7 @@ const UploadFileProblemWidget: FC<UploadFileProblemWidgetPropsType> = ({
             href={file?.link}
             component="a"
             target="_blank">
-            {file?.name}
+            {'آخرین فایل ارسالی'}
           </Button>
         }
       </Stack>
