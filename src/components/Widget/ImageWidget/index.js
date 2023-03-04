c20f71b77eb5bfc25c689dea0987757c627121b1
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-03-04 11:19:18 +0330

    remove wrapper Box tag from image and video widget

--- src/components/Widget/ImageWidget/index.js
+++ src/components/Widget/ImageWidget/index.js
@@ -1,22 +1,19 @@
-import { Box } from '@mui/material';
 import React from 'react';
 import ImageEditWidget from './edit';
 export { ImageEditWidget };
 
 const ImageWidget = ({ link, file, alt }) => {
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%' }}>
-      <img
-        alt={alt}
-        src={file || link}
-        style={{
-          maxWidth: '100%',
-          maxHeight: 500,
-          borderRadius: 10,
-          objectFit: 'contain',
-        }} />
-    </Box>
-
+    <img
+      alt={alt}
+      src={file || link}
+      style={{
+        width: '100%',
+        maxWidth: '100%',
+        maxHeight: 500,
+        objectFit: 'contain',
+        borderRadius: 10,
+      }} />
   );
 };
 
--- src/components/Widget/VideoWidget/index.js
+++ src/components/Widget/VideoWidget/index.js
@@ -1,21 +1,19 @@
-import { Box } from '@mui/material';
 import React from 'react';
 import VideoEditWidget from './edit';
 export { VideoEditWidget };
 
 const VideoWidget = ({ link, file }) => {
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%' }}>
-      <video
-        controls
-        src={file || link}
-        style={{
-          maxWidth: '100%',
-          maxHeight: 500,
-          borderRadius: 10,
-          objectFit: 'contain',
-        }} />
-    </Box>
+    <video
+      controls
+      src={file || link}
+      style={{
+        width: '100%',
+        maxWidth: '100%',
+        maxHeight: 500,
+        objectFit: 'contain',
+        borderRadius: 10,
+      }} />
   );
 };
 
