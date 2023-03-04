2a4c86bc530f24c538cbe6505beef341040a6ba9
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-03-04 10:12:02 +0330

    replace depricated ListItem

--- src/components/SpecialComponents/WorkshopPage/components/ChangeStateDialog.js
+++ src/components/SpecialComponents/WorkshopPage/components/ChangeStateDialog.js
@@ -2,7 +2,7 @@ import {
   Dialog,
   DialogTitle,
   List,
-  ListItem,
+  ListItemButton,
   Typography,
 } from '@mui/material';
 import React from 'react';
@@ -17,15 +17,15 @@ function ChangeStateDialog({ open, handleClose, edges, changeState }) {
         <Typography variant="h3">{t('chooseNextState')}</Typography>
       </DialogTitle>
       <List>
-        {edges.slice().sort((e1, e2) => e1.head.name > e2.head.name ? 1 : -1).map((edge) => (
-          <ListItem
+        {edges.slice().sort((e1, e2) => e1.head.name < e2.head.name ? 1 : -1).map((edge) => (
+          <ListItemButton
             onClick={() => {
               changeState(edge);
               handleClose();
             }}
             key={edge.id}>
             {edge.head.name}
-          </ListItem>
+          </ListItemButton>
         ))}
       </List>
     </Dialog>
