fd9eba0e296747b85d0a8544fc2c4545cca76959
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-03-04 09:43:53 +0330

    revise state outward edges

--- src/components/SpecialComponents/WorkshopPage/components/ChangeStateDialog.js
+++ src/components/SpecialComponents/WorkshopPage/components/ChangeStateDialog.js
@@ -10,21 +10,21 @@ import { useTranslate } from 'react-redux-multilingual/lib/context';
 
 function ChangeStateDialog({ open, handleClose, edges, changeState }) {
   const t = useTranslate();
+
   return (
     <Dialog open={open} onClose={handleClose}>
       <DialogTitle>
         <Typography variant="h3">{t('chooseNextState')}</Typography>
       </DialogTitle>
       <List>
-        {edges.map((edge) => (
+        {edges.slice().sort((e1, e2) => e1.head.name > e2.head.name ? 1 : -1).map((edge) => (
           <ListItem
-            button
             onClick={() => {
               changeState(edge);
               handleClose();
             }}
             key={edge.id}>
-            {edge.str}
+            {edge.head.name}
           </ListItem>
         ))}
       </List>
