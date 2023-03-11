44c3ba661fb6e9668d448abd7adfeff3616700c9
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-03-11 23:56:28 +0330

    add confetti

--- src/components/Widget/SmallAnswerProblemWidget/index.tsx
+++ src/components/Widget/SmallAnswerProblemWidget/index.tsx
@@ -7,6 +7,7 @@ import TinyPreview from '../../tiny_editor/react_tiny/Preview';
 import { WidgetModes } from '..';
 import SmallAnswerProblemEditWidget from './edit';
 import { toast } from 'react-toastify';
+import Confetti from 'react-confetti'
 
 type SmallAnswerProblemWidgetPropsType = {
   sendSmallAnswer: any;
@@ -30,6 +31,8 @@ const SmallAnswerProblemWidget: FC<SmallAnswerProblemWidgetPropsType> = ({
   const t = useTranslate();
   const [answer, setAnswer] = useState<string>(last_submitted_answer ? last_submitted_answer.text : '');
   const [disableSubmitButton, setDisableSubmitButton] = useState(false);
+  const [hasAnswered, setHasAnswered] = useState(false);
+  const [hasAnsweredCorrectly, setHasAnsweredCorrectly] = useState(false);
 
   const changeText = (e) => {
     if (mode === WidgetModes.InAnswerSheet) {
@@ -43,10 +46,13 @@ const SmallAnswerProblemWidget: FC<SmallAnswerProblemWidgetPropsType> = ({
       return;
     }
     if (mainAnswer && answer === mainAnswer.text) {
-      toast.success('آفرین! جوابت درست بود')
+      toast.success('آفرین! جوابت درست بود');
+      setHasAnswered(true);
+      setHasAnsweredCorrectly(true);
+      return;
     }
     else if (mainAnswer) {
-      toast.error('بیشتر دقت کن...')
+      setHasAnswered(true);
     } else {
       setDisableSubmitButton(true);
       setTimeout(() => {
@@ -57,54 +63,62 @@ const SmallAnswerProblemWidget: FC<SmallAnswerProblemWidgetPropsType> = ({
   }
 
   return (
-    <Stack spacing={1}>
-      <TinyPreview
-        frameProps={{
-          frameBorder: '0',
-          scrolling: 'no',
-          width: '100%',
-        }}
-        content={problemText}
-      />
-      <Stack
-        direction='row'
-        justifyContent='flex-start'
-        alignItems="stretch"
-        spacing={1}>
-        {(mode === WidgetModes.View || mode === WidgetModes.InAnswerSheet) &&
-          <>
-            <TextField
-              fullWidth
-              variant='outlined'
-              value={answer}
-              placeholder={'لطفاً پاسخ خود را وارد کنید.'}
-              onChange={changeText}
-              size="small"
-            />
-            {mode === WidgetModes.View &&
-              <Button
-                variant="outlined"
-                color="primary"
-                sx={{ whiteSpace: 'nowrap' }}
-                disabled={disableSubmitButton}
-                onClick={submit}>
-                {t('submit')}
-              </Button>
-            }
-          </>
-        }
-        {mode === WidgetModes.Review &&
-          <>
-            {answer ?
-              <Typography>{answer}</Typography> :
-              <Typography color='red' variant='caption'>
-                {'پاسخی برای این سوال ثبت نشده است.'}
-              </Typography>
-            }
-          </>
-        }
+    <>
+      {hasAnsweredCorrectly &&
+        <Confetti recycle={false} tweenDuration={10000} numberOfPieces={1500} />
+      }
+      <Stack spacing={1}>
+        <TinyPreview
+          frameProps={{
+            frameBorder: '0',
+            scrolling: 'no',
+            width: '100%',
+          }}
+          content={problemText}
+        />
+        <Stack
+          direction='row'
+          justifyContent='flex-start'
+          alignItems='stretch'
+          spacing={1}>
+          {(mode === WidgetModes.View || mode === WidgetModes.InAnswerSheet) &&
+            <>
+              <TextField
+                fullWidth
+                variant='outlined'
+                value={answer}
+                disabled={hasAnsweredCorrectly}
+                error={hasAnswered && !hasAnsweredCorrectly}
+                autoComplete='false'
+                placeholder={'لطفاً پاسخ خود را وارد کنید.'}
+                onChange={changeText}
+                size='small'
+              />
+              {mode === WidgetModes.View &&
+                <Button
+                  variant='outlined'
+                  color='primary'
+                  sx={{ whiteSpace: 'nowrap' }}
+                  disabled={disableSubmitButton || hasAnsweredCorrectly}
+                  onClick={submit}>
+                  {t('submit')}
+                </Button>
+              }
+            </>
+          }
+          {mode === WidgetModes.Review &&
+            <>
+              {answer ?
+                <Typography>{answer}</Typography> :
+                <Typography color='red' variant='caption'>
+                  {'پاسخی برای این سوال ثبت نشده است.'}
+                </Typography>
+              }
+            </>
+          }
+        </Stack>
       </Stack>
-    </Stack>
+    </>
   );
 };
 
@@ -112,11 +126,8 @@ const mapStateToProps = (state) => ({
   playerId: state.currentState.player?.id,
 });
 
-export default connect(
-  mapStateToProps,
-  {
-    sendSmallAnswer: sendSmallAnswerAction,
-  }
-)(SmallAnswerProblemWidget);
+export default connect(mapStateToProps, {
+  sendSmallAnswer: sendSmallAnswerAction,
+})(SmallAnswerProblemWidget);
 
 export { SmallAnswerProblemEditWidget };
