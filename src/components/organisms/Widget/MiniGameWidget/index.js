f45cdb0e600127dce37f65d2a3f8d2f0846734fa
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-08-05 02:39:07 +0330

    set iframe heigth responsive to width

--- src/components/organisms/Widget/MiniGameWidget/index.js
+++ src/components/organisms/Widget/MiniGameWidget/index.js
@@ -1,31 +1,23 @@
 import { Button } from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useTranslate } from 'react-redux-multilingual/lib/context';
-
 import MiniGameEditWidget from './edit';
+import { useWindowSize } from 'utils/useWindowSIze';
 
 export { MiniGameEditWidget };
 
-const useStyles = makeStyles(() => ({
-  gameWidget: {
-    width: '100%',
-    borderRadius: 10,
-    borderBottomRightRadius: 0,
-    minHeight: 300,
-    border: 'none',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.15), 0 1px 3px rgba(0, 0, 0, 0.25)',
-    maxHeight: '100vh',
-  },
-}));
-
 const GameWidget = ({ link = '' }) => {
+  const [windowWidth, _] = useWindowSize();
   const t = useTranslate();
-  const classes = useStyles();
   const iframeRef = useRef(null);
+  const [iFrameHeight, setIFrameHeight] = useState(300);
+  const ratio = 9 / 16;
 
-  const [scale, setScale] = useState(1);
-  const [iFrameHeight, setIFrameHeight] = useState(500);
+  useEffect(() => {
+    if (iframeRef.current?.scrollWidth) {
+      setIFrameHeight(ratio * iframeRef.current?.scrollWidth);
+    }
+  }, [iframeRef.current, windowWidth])
 
   const handleFullScreen = () => {
     if (iframeRef.current.requestFullscreen) {
@@ -43,25 +35,12 @@ const GameWidget = ({ link = '' }) => {
         title={t('game')}
         src={link}
         ref={iframeRef}
-        className={classes.gameWidget}
         height={iFrameHeight}
-      // style={{
-      // height: iFrameHeight,
-      // transform: `scale(${scale})`,
-      // width: `${(1 / scale) * 100}%`,
-      // marginRight: `-${((1 / scale) * 100 - 100) / 2}%`
-      // }}
-      // onLoad={(e) => {
-      //   const body =
-      //     e.target?.contentDocument?.body ??
-      //     e.target?.contentWindow?.document?.body;
-      //   setTimeout(() => {
-      //     setIFrameHeight(body.scrollHeight);
-      //     body.style.maxHeight = '100vh';
-      //     body.style.overflowY = 'auto';
-      //     body.style.overflowX = 'hidden';
-      //   }, 10);
-      // }}
+        style={{
+          width: '100%',
+          border: 'none',
+          boxShadow: '0 2px 4px rgba(0, 0, 0, 0.15), 0 1px 3px rgba(0, 0, 0, 0.25)',
+        }}
       />
       <Button sx={{ borderTopLeftRadius: 0, borderTopRightRadius: 0 }} variant='outlined' onClick={handleFullScreen}>
         {'حالت تمام صفحه'}
