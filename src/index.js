8402d3f038c59e0cf548347fceedf12b874b6d32
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-08-10 17:05:45 +0330

    fix sentry

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+import { BrowserTracing } from "@sentry/browser";
+
 import App from './App';
 import reduxStore from './redux/store';
 
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
-    dsn: "https://1101899554bf45c48a4cd2e6a7e26ac2@sentry.hamravesh.com/368",
-    integrations: [new Integrations.BrowserTracing()],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
+    dsn: process.env.SENTRY_DNS,
+    integrations: [new BrowserTracing()],
     tracesSampleRate: 1.0,
   });
 }
