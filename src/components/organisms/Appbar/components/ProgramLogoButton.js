37cae9dc4463d0de4696e152625a50f84587f737
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-09-28 00:40:42 +0330

    add tooltip for program logo button

--- src/components/organisms/Appbar/components/ProgramLogoButton.js
+++ src/components/organisms/Appbar/components/ProgramLogoButton.js
@@ -1,4 +1,4 @@
-import { Button, IconButton, Stack, Typography } from '@mui/material';
+import { Button, IconButton, Stack, Tooltip, Typography } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import useWidth from 'utils/UseWidth';
@@ -7,19 +7,21 @@ function ProgramLogoButton({ image, name, programId }) {
   const width = useWidth();
   return (
     <Stack direction={'row'} alignItems={'center'}>
-      <IconButton disabled={width !== 'xs'} component={Link} to={`/program/${programId}/`}>
-        <img
-          src={image}
-          alt='course-logo'
-          style={{
-            objectFit: 'cover',
-            borderRadius: '50%',
-            height: 40,
-            width: 40,
-            border: '1px solid #00000099',
-          }}
-        />
-      </IconButton>
+      <Tooltip title={name} arrow>
+        <IconButton disabled={width !== 'xs'} component={Link} to={`/program/${programId}/`}>
+          <img
+            src={image}
+            alt='course-logo'
+            style={{
+              objectFit: 'cover',
+              borderRadius: '50%',
+              height: 40,
+              width: 40,
+              border: '1px solid #00000099',
+            }}
+          />
+        </IconButton>
+      </Tooltip>
       <Button sx={{ display: { xs: 'none', sm: 'inline' }, paddingLeft: 0 }} component={Link} to={`/program/${programId}/`}>
         <Typography fontSize={20} fontWeight={440} color={'black'}>
           {name}
