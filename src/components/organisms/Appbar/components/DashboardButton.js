1bf6a193a58f20263cb705328216041d4f3e3f56
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-09-27 22:29:26 +0330

    refactor DashboardButton

--- src/components/organisms/Appbar/components/DashboardButton.js
+++ src/components/organisms/Appbar/components/DashboardButton.js
@@ -1,39 +1,19 @@
 import { Button, Icon, Typography } from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const useStyles = makeStyles(() => ({
-  iconImage: {
-    maxHeight: '20px',
-    width: '100%',
-  },
-}));
-
-export default function DashboardButton({ name, iconImage, to, onClick }) {
+function DashboardButton({ name, to, onClick }) {
   const navigate = useNavigate();
-  const classes = useStyles();
+
   return (
     <Button
-      variant="outlined"
       onClick={() => {
         navigate(to);
         onClick?.();
-      }}
-      startIcon={
-        iconImage ? (
-          <Icon>
-            <img
-              src={`${process.env.PUBLIC_URL}/ZeroJourneyer/IconImages/${iconImage}`}
-              alt="iconImage"
-              className={classes.iconImage}
-            />
-          </Icon>
-        ) : (
-          ''
-        )
-      }>
-      <Typography variant="caption">{name}</Typography>
+      }}>
+      <Typography fontSize={14} fontWeight={400}>{name}</Typography>
     </Button>
   );
 }
+
+export default DashboardButton;
