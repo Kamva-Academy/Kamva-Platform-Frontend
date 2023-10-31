31c6f192449afd2547f4f3acdb6353b81a45cf7b
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-11-01 01:57:25 +0330

    refactor program page sidebar

--- src/pages/Event/Sidebar.js
+++ src/pages/Event/Sidebar.js
@@ -1,61 +1,18 @@
-import { Button, Container, Grid, Paper, Typography, Divider, Box, Stack } from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import TextField from '@mui/material/TextField';
-import React, { useEffect, useState } from 'react';
+import { Button, Grid, Paper, Typography, Divider, Stack } from '@mui/material';
+import React from 'react';
 import { connect } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
 import downloadFile from 'utils/downloadFile';
 
 import {
   getCertificateAction,
-  getOneEventInfoAction,
-  getOneRegistrationFormAction,
 } from 'redux/slices/events';
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: theme.spacing(3),
-  },
-  customAmountBox: {
-    display: 'none',
-  },
-}));
-
-function Workshops({
+function ProgramPageSidebar({
   getCertificate,
-  getOneEventInfo,
-  getOneRegistrationForm,
-  registrationForm,
   event,
 }) {
   const navigate = useNavigate();
-  const classes = useStyles();
-  const { programId } = useParams();
-  const [amount, setAmount] = React.useState('');
-
-  useEffect(() => {
-    getOneEventInfo({ programId });
-  }, []);
-
-  const handleAmountChange = (event) => {
-    const box = document.getElementById('customAmount');
-    if (event.target.value == 'Custom') {
-      box.style.display = 'unset';
-      setAmount(50000);
-    } else {
-      box.style.display = 'none';
-      setAmount(event.target.value);
-    }
-  };
-
-  const handleTextboxAmountChange = (event) => {
-    setAmount(parseInt(event.target.value));
-  };
 
   const doGetCertificate = () => {
     getCertificate({ registrationReceiptId: event?.registration_receipt }).then(
@@ -67,91 +24,39 @@ function Workshops({
     );
   };
 
-  const payDonation = () => {
-    console.log(amount);
-  };
-
   return (
     <>
       <Grid item>
-        <Paper className={classes.paper}>
-          <Stack spacing={2} alignItems="center">
-            <Typography variant="h5">
-              {`به ${event?.name || ''} خوش آمدید!`}
-            </Typography>
+        <Stack spacing={2} alignItems="center" component={Paper} padding={2}>
+          <Typography variant="h5">
+            {`به ${event?.name || ''} خوش آمدید!`}
+          </Typography>
+          <Button
+            // disabled
+            disabled={event?.event_type == 'Individual'}
+            variant="outlined"
+            fullWidth
+            onClick={() => navigate(`/program/${event?.id}/team-selection/`)}>
+            {'گروه‌بندی'}
+          </Button>
+          <Button
+            disabled={!event?.has_certificate || !event?.certificates_ready}
+            onClick={doGetCertificate}
+            variant="outlined"
+            fullWidth>
+            {'گواهی حضور'}
+          </Button>
+          {event.is_manager && <>
+            <Divider width='100%'></Divider>
             <Button
-              // disabled
-              disabled={event?.event_type == 'Individual'}
-              variant="outlined"
+              variant="contained"
               fullWidth
-              onClick={() => navigate(`/program/${event?.id}/team-selection/`)}>
-              {'گروه‌بندی'}
+              onClick={() => navigate(`/program/${event?.id}/manage/info`)}>
+              {'مدیریت دوره'}
             </Button>
-            <Button
-              disabled={!event?.has_certificate || !event?.certificates_ready}
-              onClick={doGetCertificate}
-              variant="outlined"
-              fullWidth>
-              {'گواهی حضور'}
-            </Button>
-            {event.is_manager && <>
-              <Divider width='100%'></Divider>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={() => navigate(`/program/${event?.id}/manage/info`)}>
-                {'مدیریت دوره'}
-              </Button>
-            </>}
-          </Stack>
-        </Paper>
+          </>}
+        </Stack>
       </Grid>
-      {/* <Grid item>
-        <Paper className={classes.paper}>
-          <Grid container spacing={2} alignItems="center">
-            <Grid item xs={12}>
-              <Typography variant="h5">{'کمک مالی'}</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl fullWidth variant="filled" className={classes.formControl}>
-                <InputLabel id="demo-simple-select-filled-label">مقدار</InputLabel>
-                <Select
-                  labelId="demo-simple-select-filled-label"
-                  id="demo-simple-select-filled"
-                  value={amount}
-                  onChange={handleAmountChange}>
-                  <MenuItem value={10000}>ده هزار تومان</MenuItem>
-                  <MenuItem value={20000}>بیست هزار تومان</MenuItem>
-                  <MenuItem value={30000}>سی هزار تومان</MenuItem>
-                  <MenuItem value={'Custom'}>مقدار دیگر</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <form
-                className={classes.customAmountBox}
-                noValidate
-                autoComplete="off"
-                fullWidth
-                id={'customAmount'}>
-                <TextField
-                  id="standard-basic"
-                  fullWidth
-                  value={amount}
-                  type="number"
-                  onChange={handleTextboxAmountChange}
-                />
-                <Typography variant="p">{'تومان'}</Typography>
-              </form>
-            </Grid>
-            <Grid item xs={12}>
-              <Button onClick={payDonation} variant="outlined" fullWidth>
-                {'پرداخت'}
-              </Button>
-            </Grid>
-          </Grid>
-        </Paper>
-      </Grid> */}
     </>
   );
 }
@@ -163,6 +68,4 @@ const mapStateToProps = (state) => ({
 
 export default connect(mapStateToProps, {
   getCertificate: getCertificateAction,
-  getOneEventInfo: getOneEventInfoAction,
-  getOneRegistrationForm: getOneRegistrationFormAction,
-})(Workshops);
+})(ProgramPageSidebar);
