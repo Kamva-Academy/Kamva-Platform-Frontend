b5d187f44a588c755b672360a8474cf90efaf7a3
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-11-13 17:20:01 +0330

    fix type

--- src/utils/downloadFile.tsx
+++ src/utils/downloadFile.tsx
@@ -1,11 +1,11 @@
-const downloadFile = (fileUrl: string, filename: string, filMimeType: string) => {
+const downloadFile = (fileUrl: string, filename: string, fileMimeType: string) => {
   var request = new XMLHttpRequest();
   request.open('GET', fileUrl, true);
   request.responseType = 'blob';
 
   request.onload = function () {
     if (request.status === 200) {
-      var blob = new Blob([request.response], { type: filMimeType });
+      var blob = new Blob([request.response], { type: fileMimeType });
       var link = document.createElement('a');
       link.href = window.URL.createObjectURL(blob);
       link.download = filename;
