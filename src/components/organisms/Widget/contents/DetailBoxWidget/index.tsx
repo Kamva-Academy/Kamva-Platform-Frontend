1c8750ae173d5d1616147d51756b683eeaf37071
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-12-22 19:06:28 +0330

    make DetailBox collapsable

--- src/components/organisms/Widget/contents/DetailBoxWidget/index.tsx
+++ src/components/organisms/Widget/contents/DetailBoxWidget/index.tsx
@@ -1,23 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import TinyPreview from 'components/tiny_editor/react_tiny/Preview';
 import DetailBoxEditDialog from './EditDialog';
 import Paper from 'components/template/Paper';
+import { Box, Collapse, IconButton, Stack } from '@mui/material';
+import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
 
 export { DetailBoxEditDialog };
 
 const DetailBoxWidget = ({ title, details }) => {
+  const [openRoadMap, setOpenRoadMap] = useState(false);
+
   return (
     <Fragment>
-      <TinyPreview
-        frameProps={{
-          frameBorder: '0',
-          scrolling: 'no',
-          width: '100%',
-        }}
-        content={title}
-      />
-      <Paper paper={details} />
+      <Stack direction={'row'}>
+        <IconButton onClick={() => setOpenRoadMap(!openRoadMap)} sx={{ padding: 0, marginRight: 1, marginTop: -1 }}>
+          <ArrowDropDownCircleIcon sx={{ transform: openRoadMap ? 'rotate(-180deg)' : null }} />
+        </IconButton>
+        <Box>
+          <TinyPreview
+            frameProps={{
+              frameBorder: '0',
+              scrolling: 'no',
+              width: '100%',
+            }}
+            content={title}
+          />
+        </Box>
+      </Stack>
+      <Collapse in={openRoadMap}>
+        <Paper paper={details} />
+      </Collapse>
     </Fragment>
   );
 };
