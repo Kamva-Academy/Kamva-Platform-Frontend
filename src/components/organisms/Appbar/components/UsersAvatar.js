35f66a159ce12c1b1f4800e18cd96809b616bfc4
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-01-07 19:42:39 +0330

    fix some bugs in appbar

--- src/components/organisms/Appbar/components/UsersAvatar.js
+++ src/components/organisms/Appbar/components/UsersAvatar.js
@@ -2,12 +2,14 @@ import {
   Avatar,
   Badge,
   Box,
+  Button,
+  IconButton,
   Menu,
   MenuItem,
   Tooltip,
 } from '@mui/material';
 import { OfflineBolt } from '@mui/icons-material';
-import React, { useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 
 import { StatePageContext } from 'pages/FSM';
 import { stringToColor } from 'utils/stringToColor';
@@ -26,59 +28,60 @@ const UsersAvatar = () => {
     setAnchorEl(null);
   };
 
-  return <>
-    <Tooltip
-      onClick={handleClick}
-      arrow
-      title={`گروه ${myTeam?.name}`}>
-      <Avatar
-        style={{
-          backgroundColor: stringToColor(`گروه ${myTeam?.name}`),
-          border: '0.1px solid lightgray',
-        }}>
-        {myTeam?.name.trim()[0]}
-      </Avatar>
-    </Tooltip>
-
-    <Menu disableScrollLock
-      anchorEl={anchorEl}
-      keepMounted
-      open={open}
-      onClose={handleClose}
-    >
-      <MenuItem>
-        {`گروه ${myTeam?.name ? myTeam?.name : '؟'}`}
-      </MenuItem>
-
-      {myTeam?.members?.map((member) =>
-        <MenuItem key={member.id}>
-          <Badge
-            key={member.id}
-            overlap="circular"
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            invisible={myTeam?.team_head !== member.id}
-            badgeContent={<OfflineBolt style={{ color: 'gold' }} />}>
-            <Avatar
-              style={{
-                backgroundColor: stringToColor(
-                  `${member.first_name} ${member.last_name}`
-                ),
-                border: '0.1px solid lightgray',
-              }}>
-              {`${member.first_name ? member.first_name[0] : '؟'}`}
-            </Avatar>
-          </Badge>
-          <Box ml={1}>
-            {`${member.first_name ? member.first_name : 'بی‌نام'} ${member.last_name ? member.last_name : 'بی‌نام‌زاده'}`}
-          </Box>
+  return (
+    <Fragment>
+      <Tooltip
+        components={Button}
+        onClick={handleClick}
+        arrow
+        title={`گروه ${myTeam?.name}`}>
+        <IconButton sx={{ padding: 0 }}>
+          <Avatar
+            sx={{
+              backgroundColor: stringToColor(`گروه ${myTeam?.name}`),
+              border: '0.1px solid lightgray',
+            }}>
+            {myTeam?.name.trim()[0]}
+          </Avatar>
+        </IconButton>
+      </Tooltip>
+      <Menu disableScrollLock
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}>
+        <MenuItem>
+          {`گروه ${myTeam?.name ? myTeam?.name : '؟'}`}
         </MenuItem>
-
-      )}
-    </Menu>
-  </>;
+        {myTeam?.members?.map((member) =>
+          <MenuItem key={member.id}>
+            <Badge
+              key={member.id}
+              overlap="circular"
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              invisible={myTeam?.team_head !== member.id}
+              badgeContent={<OfflineBolt style={{ color: 'gold' }} />}>
+              <Avatar
+                style={{
+                  backgroundColor: stringToColor(
+                    `${member.first_name} ${member.last_name}`
+                  ),
+                  border: '0.1px solid lightgray',
+                }}>
+                {`${member.first_name ? member.first_name[0] : '؟'}`}
+              </Avatar>
+            </Badge>
+            <Box ml={1}>
+              {`${member.first_name ? member.first_name : 'بی‌نام'} ${member.last_name ? member.last_name : 'بی‌نام‌زاده'}`}
+            </Box>
+          </MenuItem>
+        )}
+      </Menu>
+    </Fragment>
+  );
 };
 
 export default UsersAvatar;
--- src/components/organisms/Appbar/modes/FSMAppbarItems.js
+++ src/components/organisms/Appbar/modes/FSMAppbarItems.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 
-import DashboardButton from '../components/DashboardButton';
 import ChatRoomButton from '../components/ChatRoomButton';
 import MentorButton from '../components/MentorButton';
 import ReviewAnswersButton from '../components/ReviewAnswersButton';
@@ -24,7 +23,6 @@ const FSMAppbarItems = ({ program, workshop, isMentor }) => {
   const { programId } = useParams();
   const reviewAnswers = <ReviewAnswersButton />
   const chatRoomButton = <ChatRoomButton />;
-  const backToEventButton = <DashboardButton name={'بازگشت'} to={`/program/${programId}/`} />;
   const whiteboardButton = <WhiteboardButton />;
   const mentorButton = <MentorButton />;
   const teamAvatar = <TeamAvatar />;
@@ -32,9 +30,9 @@ const FSMAppbarItems = ({ program, workshop, isMentor }) => {
   const scoresDialogButton = <ScoresDialogButton />
 
   const desktopLeftItems = [];
-  const desktopRightItems = [];
+  const desktopRightItems = [programLogoButton];
   const mobileLeftItems = [];
-  const mobileRightItems = [];
+  const mobileRightItems = [programLogoButton];
   const mobileMenuListItems = [];
 
   if (workshop?.first_state?.is_exam) {
@@ -42,23 +40,23 @@ const FSMAppbarItems = ({ program, workshop, isMentor }) => {
     mobileMenuListItems.push(reviewAnswers);
   }
 
-  if (workshop?.fsm_p_type == 'Team') {
-    desktopRightItems.push(teamAvatar);
+  if (program?.show_scores) {
+    desktopLeftItems.push(scoresDialogButton);
+    mobileMenuListItems.push(scoresDialogButton);
   }
 
   if (workshop?.fsm_learning_type == 'Supervised') {
-    desktopLeftItems.push([whiteboardButton, mentorButton]);
-    desktopRightItems.push([chatRoomButton]);
-    mobileLeftItems.push([whiteboardButton, mentorButton]);
-    mobileRightItems.push([chatRoomButton]);
-  } else {
-    mobileRightItems.push(programLogoButton);
-    desktopRightItems.push(programLogoButton);
+    desktopLeftItems.push(mentorButton);
+    mobileLeftItems.push(mentorButton);
   }
 
-  if (program.show_scores) {
-    desktopLeftItems.push(scoresDialogButton);
-    mobileMenuListItems.push(scoresDialogButton);
+  if (workshop?.fsm_learning_type == 'Supervised' || workshop?.fsm_p_type == 'Team') {
+    desktopLeftItems.push(whiteboardButton, chatRoomButton);
+    mobileRightItems.push(whiteboardButton, chatRoomButton);
+  }
+
+  if (workshop?.fsm_p_type == 'Team') {
+    desktopLeftItems.push(teamAvatar);
   }
 
   return {
