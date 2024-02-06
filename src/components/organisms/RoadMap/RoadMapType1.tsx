b0d10ad94a4e2b163fc5ad34042b19208bc4c462
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-02-06 17:08:09 +0330

    fix arranging nodes in graph

--- src/components/organisms/RoadMap/RoadMapType1.tsx
+++ src/components/organisms/RoadMap/RoadMapType1.tsx
@@ -16,27 +16,40 @@ const GraphType1: FC<GraphType1PropsType> = ({
   firstStateName,
 }) => {
   const nodesXdistance = 120;
-  const nodesYdistance = 200;
+  const nodesYdistance = 100;
+  const graph = {};
 
-  const nodes = [];
-  const links = [];
+  const getRandomBoolean = () => Math.random() > 0.5;
 
-  const getY = (initialY) => initialY + Math.floor(nodesYdistance * Math.random() - (nodesYdistance / 2))
+  const handleAddingNode = (id: string, x: number, nodes: Node[]) => {
+    let y = null;
+    if (!graph[x]) {
+      y = (getRandomBoolean() ? 1 : -1) * Math.floor(nodesYdistance * Math.random());
+      graph[x] = [y];
+    } else {
+      if (getRandomBoolean()) {
+        y = graph[x][0] - nodesYdistance;
+        graph[x].unshift(y);
+      } else {
+        y = graph[x][graph[x].length - 1] + nodesYdistance;
+        graph[x].push(y);
+      }
+    }
+    nodes.push({ id, x, y });
+  }
 
   const handleAddingLink = (sourceId: string, targetId: string, nodes: Node[], links: Link[]) => {
     let sourceNode = nodes.find(node => node.id === sourceId);
     let targetNode = nodes.find(node => node.id === targetId);
 
     if (sourceNode && !targetNode) {
-      targetNode = { id: targetId, x: sourceNode.x - nodesXdistance, y: getY(sourceNode.y) };
-      nodes.push(targetNode);
+      handleAddingNode(targetId, sourceNode.x - nodesXdistance, nodes);
       links.push({
         source: sourceId,
         target: targetId,
       })
     } else if (!sourceNode && targetNode) {
-      sourceNode = { id: sourceId, x: targetNode.x + nodesXdistance, y: getY(targetNode.y) };
-      nodes.push(sourceNode);
+      handleAddingNode(sourceId, targetNode.x + nodesXdistance, nodes);
       links.push({
         source: sourceId,
         target: targetId,
@@ -53,36 +66,31 @@ const GraphType1: FC<GraphType1PropsType> = ({
     }
   }
 
-  const developGraph = () => {
-    // check weather inputLinks are updated and are not cached in the redux from previous fsm
-    if (!inputLinks.find(link => link.source === currentNodeId || link.target === currentNodeId)) {
-      return;
-    }
-
-    let firstNode = nodes.find(node => node.id === firstStateName);
-
-    if (!firstNode) {
-      firstNode = { id: firstStateName, x: 0, y: getY(0) };
-      nodes.push(firstNode);
-    }
-
-    const inputLinksCopy = [...inputLinks];
+  const developGraph = (inputLinks: any[]) => {
+    const nodes = [];
+    const links = [];
+    handleAddingNode(firstStateName, 0, nodes);
 
     let counter = 10_000;
-    while (inputLinksCopy.length > 0 && counter--) {
-      const link = inputLinksCopy[0];
+    while (inputLinks.length > 0 && counter--) {
+      const link = inputLinks[0];
       let sourceNode = nodes.find(node => node.id === link.source);
       let targetNode = nodes.find(node => node.id === link.target);
       if (!sourceNode && !targetNode) {
-        inputLinksCopy.push(inputLinksCopy.shift());
+        inputLinks.push(inputLinks.shift());
       } else {
         handleAddingLink(link.source, link.target, nodes, links);
-        inputLinksCopy.shift();
+        inputLinks.shift();
       }
     }
+    return [nodes, links]
   }
 
-  developGraph();
+  // check weather inputLinks are updated and are not cached in the redux from previous fsm
+  if (!inputLinks.find(link => link.source === currentNodeId || link.target === currentNodeId)) {
+    return;
+  }
+  const [nodes, links] = developGraph([...inputLinks]);
 
   return (
     <BaseGraph height={200} dragAndDrop={false} currentNodeId={currentNodeId} nodes={nodes} links={links} highlighedPath={highlighedPath} />
