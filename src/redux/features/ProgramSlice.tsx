41b9e3d6e1202cdf8831155e46aa52ca53862901
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-02-23 01:58:46 +0330

    change get programs respone format

--- src/redux/features/ProgramSlice.tsx
+++ src/redux/features/ProgramSlice.tsx
@@ -4,9 +4,12 @@ import { mainBackendBaseApi } from './mainBackendApiSlice';
 export const ProgramSlice = mainBackendBaseApi.injectEndpoints({
   endpoints: builder => ({
     getPrograms: builder.query<ProgramType[], string | undefined>({
-      query: (partyUuid) => partyUuid ? `fsm/event/?party_uuid=${partyUuid}` : null,
+      query: (partyUuid) => partyUuid ? `fsm/event/?party=${partyUuid}&is_private=False` : null,
+      transformResponse: (respons: any): ProgramType[] => {
+        return respons.results;
+      },
     })
   })
-})
+});
 
 export const { useGetProgramsQuery } = ProgramSlice;
