df3d6041b5f8547fabce15ce2aeb8ee63ce40534
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-02-24 03:35:24 +0330

    fix browser tab title

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -21,24 +21,22 @@ const Layout: FC<LayoutPropsType> = ({
 
   const { data: party } = useGetPartyQuery();
 
-  if (!party) {
-    return null;
-  }
-
   return (
     <Fragment>
-      <Helmet>
-        <title>{party.main_page_header_data.title}</title>
-        <link rel="icon" href={party.logo.mobile_image} />
-        <meta name="description" content={party.main_page_header_data.description} />
-        <meta name="theme-color" content={party.main_page_header_data.theme_color} />
+      {party &&
+        <Helmet>
+          <title>{party.main_page_header_data.title}</title>
+          <link rel="icon" href={party.logo.mobile_image} />
+          <meta name="description" content={party.main_page_header_data.description} />
+          <meta name="theme-color" content={party.main_page_header_data.theme_color} />
 
-        <meta property="og:title" content={party.main_page_og_metadata.title} />
-        <meta property="og:description" content={party.main_page_og_metadata.description} />
-        <meta property="og:type" content={party.main_page_og_metadata.type} />
-        <meta property="og:image" content={party.main_page_og_metadata.image} />
-        <meta property="og:url" content={party.main_page_og_metadata.url} />
-      </Helmet>
+          <meta property="og:title" content={party.main_page_og_metadata.title} />
+          <meta property="og:description" content={party.main_page_og_metadata.description} />
+          <meta property="og:type" content={party.main_page_og_metadata.type} />
+          <meta property="og:image" content={party.main_page_og_metadata.image} />
+          <meta property="og:url" content={party.main_page_og_metadata.url} />
+        </Helmet>
+      }
       <AppBar mode={appbarMode} position="relative" />
       <Container maxWidth='lg'
         sx={{
