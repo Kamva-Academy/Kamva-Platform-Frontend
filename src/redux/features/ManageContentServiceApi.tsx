a210d9cc92679f1ea8027972bf242cbf5b8bcd87
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-02-24 04:27:52 +0330

    remove dead code

--- src/redux/features/ManageContentServiceApi.tsx
+++ src/redux/features/ManageContentServiceApi.tsx
@@ -18,23 +18,5 @@ export const ManageContentServiceApi = createApi({
     },
   }),
   endpoints: build => ({
-    // // A query endpoint with no arguments
-    // getTodos: build.query({
-    //   query: () => '/todos'
-    // }),
-    // // A query endpoint with an argument
-    // userById: build.query({
-    //   query: userId => `/users/${userId}`
-    // }),
-    // // A mutation endpoint
-    // updateTodo: build.mutation({
-    //   query: updatedTodo => ({
-    //     url: `/todos/${updatedTodo.id}`,
-    //     method: 'POST',
-    //     body: updatedTodo
-    //   })
-    // })
   })
 })
-
-// export const { useGetTodosQuery, useUserByIdQuery, useUpdateTodoMutation } = api;
--- src/redux/features/apiSlice.tsx
+++ src/redux/features/apiSlice.tsx
@@ -8,23 +8,5 @@ export const api = createApi({
     baseUrl: 'https://mps.sepid.org/api/',
   }),
   endpoints: build => ({
-    // // A query endpoint with no arguments
-    // getTodos: build.query({
-    //   query: () => '/todos'
-    // }),
-    // // A query endpoint with an argument
-    // userById: build.query({
-    //   query: userId => `/users/${userId}`
-    // }),
-    // // A mutation endpoint
-    // updateTodo: build.mutation({
-    //   query: updatedTodo => ({
-    //     url: `/todos/${updatedTodo.id}`,
-    //     method: 'POST',
-    //     body: updatedTodo
-    //   })
-    // })
   })
 })
-
-// export const { useGetTodosQuery, useUserByIdQuery, useUpdateTodoMutation } = api;
