d6d08d8932817e6e83c4103223536614473749ee
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-02-27 00:59:43 +0330

    replace lamp image with help chip

--- src/components/molecules/WidgetHint.tsx
+++ src/components/molecules/WidgetHint.tsx
@@ -1,6 +1,8 @@
 import { Box, Chip } from '@mui/material';
 import { Help as HelpIcon } from '@mui/icons-material';
-import React, { FC, useState } from 'react';
+import React, { FC, Fragment, useState } from 'react';
+import { Button, Fab, IconButton, Tooltip, Typography } from '@mui/material';
+
 
 import HelpDialog from 'components/organisms/dialogs/FSMStateHelpDialog';
 
@@ -10,30 +12,31 @@ type WidgetHintPropsType = {
 
 const WidgetHint: FC<WidgetHintPropsType> = ({ hints }) => {
   const [openViewHintDialog, setViewHintDialog] = useState(false);
-  const [hasClickedHintDialog, setClickedHintDialog] = useState(false);
+  const [isHover, setIsHover] = useState(false);
 
   return (
-    <>
-      <Box sx={{ position: 'absolute', right: -20, top: -10, rotate: '-24deg' }}>
-        <Chip
-          size='small'
-          color='secondary'
-          sx={{
-            color: 'white',
-            opacity: 0.4,
-            animation: !hasClickedHintDialog ? "starred 15s infinite" : 'none',
-            ":hover": { opacity: 1, animation: 'none' },
-          }}
-          onClick={() => { setViewHintDialog(true); setClickedHintDialog(true); }}
-          icon={<HelpIcon fontSize='small' />}
-          label="راهنمایی" />
+    <Fragment>
+      <Box sx={{ position: 'absolute', right: -10, top: -18, rotate: '-24deg' }}>
+        <Tooltip arrow title='راهنمایی'>
+          <IconButton
+            sx={{
+              opacity: 0.4,
+              ":hover": { opacity: 1 },
+            }}
+            disableRipple
+            onMouseOver={() => setIsHover(true)}
+            onMouseOut={() => setIsHover(false)}
+            onClick={() => { setViewHintDialog(true); }}>
+            <img width={40} src={process.env.PUBLIC_URL + ((isHover || openViewHintDialog) ? '/images/idea-on.png' : '/images/idea-off.png')} />
+          </IconButton>
+        </Tooltip>
       </Box>
       <HelpDialog
         open={openViewHintDialog}
         handleClose={() => setViewHintDialog(false)}
         helps={hints}
       />
-    </>
+    </Fragment >
 
   );
 };
